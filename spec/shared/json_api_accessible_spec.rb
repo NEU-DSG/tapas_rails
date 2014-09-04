f55c41f4704d19b01f8a708753d96810cc1558eb
Will Jackson <wjackson64@gmail.com>
2014-09-04 15:44:39 -0400

    Eliminate pending from json_api_accessible spec.

--- spec/shared/json_api_accessible_spec.rb
+++ spec/shared/json_api_accessible_spec.rb
@@ -2,11 +2,6 @@ require 'spec_helper'
 
 shared_examples_for "a JSON accessible API" do 
   let(:user) { FactoryGirl.create(:user) } 
-  before(:all) do 
-    Rails.configuration.action_controller.allow_forgery_protection = true
-  end
-
-  pending "Figure out how to test CSRF enable/disable"
 
   it "403s for requests with no attached user" do 
     post :create, { token: "blah", format: "json" } 
@@ -23,12 +18,10 @@ shared_examples_for "a JSON accessible API" do
     expect(response.status).to eq 403
   end
 
+  # Don't verify that the action does the right thing, since this request 
+  # is probably still bupkes.  Just verify that it doesn't 403.
   it "allows requests with a valid token" do 
     post :create, { email: user.email, token: "test_api_key", format: "json" }
     expect(response.status).not_to eq 403
-  end
-
-  after(:all) do 
-    Rails.configuration.action_controller.allow_forgery_protection = false 
   end
 end
