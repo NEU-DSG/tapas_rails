f3c2e8a62b2d085c3de41f1b337220bef41c6e5d
Will Jackson <wjackson64@gmail.com>
2014-09-08 12:11:24 -0400

    Skeleton of creator service business.

---
+++ app/services/community_creator_service.rb
@@ -0,0 +1,1 @@
+class CommunityCreatorService ; end
--- app/services/core_file_creator_service.rb
+++ app/services/core_file_creator_service.rb
@@ -1,3 +1,15 @@
 class CoreFileCreatorService
-  #TODO: Implement this after Validators are done
+  attr_accessor :params
+
+  def initialize(params)
+    @params = params
+  end
+
+  def self.create_record(params) 
+    self.new(params).create_record
+  end
+
+  def create_record 
+    #TODO
+  end
 end
