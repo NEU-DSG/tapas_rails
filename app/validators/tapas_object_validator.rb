0dcf170691c710330af5d19aad98c65990f69572
Will Jackson <wjackson64@gmail.com>
2014-11-05 17:18:32 +0000

    Remove unused and probably unusable tapas_object_validation method.

--- app/validators/tapas_object_validator.rb
+++ app/validators/tapas_object_validator.rb
@@ -28,22 +28,4 @@ module TapasObjectValidator
       end
     end
   end
-
-  # Requires that a method 'expected_parent_classes' be defined on the 
-  # containing class that returns an array of class constants
-  def validate_parent_helper(expected_parent_classes)
-    pid = params[:parent]
-
-    if ActiveFedora::Base.exists?(pid) && pid
-      parent = ActiveFedora::Base.find(pid, cast: true)
-
-      unless expected_parent_classes.include? parent.class 
-        errors << %W(Object at specified parent pid #{pid} was a 
-                  #{parent.class}.  Must be one of: 
-                  #{expected_parent_classes}.).join(" ")
-      end
-    elsif pid
-      errors << "No object exists at the specified parent pid of #{pid}"
-    end
-  end
 end
