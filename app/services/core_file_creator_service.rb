c97cde0faa6e6dea9f79d75fbdbf35d0e081a5c2
Will Jackson <wjackson64@gmail.com>
2014-11-13 21:02:05 +0000

    First pass at core_file creation from the information we receive from Drupal.  Note that the multiple saves of each object are actually a system requirement - the RELS-EXT datastream fails to populate correctly with non-persisted objects that have rels defined on them.

--- app/services/core_file_creator_service.rb
+++ app/services/core_file_creator_service.rb
@@ -1,30 +1,61 @@
 class CoreFileCreatorService
   include Concerns::TapasObjectCreator
 
-  def create_record 
+  def create_record(run_cleanup = true) 
     # Things to do:
       # 1. Instantiate record with pid assigned
       # 2. Assign metadata from all params
       # 3. If a TEI-XML object type is provided, extract the TEI header
       # 4. Assign all file objects to their own content_file objects
       # 5. Return a 201 to some endpoint on the drupal site. 
+    begin 
+      core     = nil 
+      tei_file = nil
 
-    core     = nil 
-    tei_file = nil
+      # 1. Instantiate a core record 
+      core = CoreFile.new
 
-    # 1. Instantiate a core record 
-    core = CoreFile.new
+      # 2. Assign metadata from all params
+      core.depositor        = params[:depositor]
+      core.mass_permissions = "private" 
+      core.nid              = params[:node_id]
+     
+      core.save!
 
-    # 2. Assign metadata from all params
-    core.depositor  = params[:depositor]
-    # TODO: How do collections work now?
+      # Attach this file to its collection, or add it to the phantom 
+      # collection if it doesn't seem to have one.
+      collection = Collection.find_by_nid(params[:collection_id])
+      if collection
+        core.collection_id = collection.id
+      else
+        core.collection_id = Collection.phantom_collection.pid
+      end
 
-    # Take params[:file] (always assumed to be a string file path as 
-    # this makes enqueing this in a job possible), extract it to its 
-    # own TEIFile object, and kick off inline derivation creation
-    tei_file = TEIFile.new(depositor: params[:depositor])
-    file_str = File.read(params[:file])
-    tei_file.content = file_str
-    FileUtils.rm(params[:file])
+      core.save!
+
+      # Take params[:file] (always assumed to be a string file path as 
+      # this makes enqueing this in a job possible), extract it to its 
+      # own TEIFile object, and kick off inline derivation creation
+      tei_file = TEIFile.new(depositor: params[:depositor])
+
+      fname = Pathname.new(params[:file]).basename.to_s
+      fblob = File.read(params[:file])
+      tei_file.add_file(fblob, "content", fname)
+      tei_file.depositor = params[:depositor]
+      tei_file.canonize
+      tei_file.save!
+      tei_file.core_file = core
+      tei_file.save!
+      return core
+    rescue => e
+      # If the process doesn't complete, ensure files are cleaned out of the 
+      # repository and that an email is sent (production environments only).
+      core.destroy if core && core.persisted? 
+      tei_file.destroy if tei_file && tei_file.persisted?
+      raise e
+    ensure
+      # Always ensure that the file at params[:file] is deleted.
+      FileUtils.rm(params[:file]) if run_cleanup
+    end
   end
 end
--- spec/services/core_file_creator_service_spec.rb
+++ spec/services/core_file_creator_service_spec.rb
@@ -1,5 +1,66 @@
 require 'spec_helper'
 
 describe CoreFileCreatorService do 
-  pending "write some tests once this actually does something" 
+  describe "A clean run" do 
+    before(:all) do 
+      @original_path = "#{Rails.root}/spec/fixtures/files/tei.xml"
+      @copy_path     = "#{Rails.root}/spec/fixtures/files/tei_copy.xml"
+
+      FileUtils.cp(@original_path, @copy_path)
+
+      params = {}
+      params[:depositor]     = "tapasguy@brown.edu"
+      params[:node_id]       = "123"
+      params[:collection_id] = "3017"
+      params[:file]          = @copy_path
+
+      # Instantiate the collection we expect to exist
+      @collection = Collection.new
+      @collection.nid       = "3017"
+      @collection.depositor = "tapasguy@brown.edu"
+      @collection.save!
+
+      @core = CoreFileCreatorService.create_record(params)
+    end
+
+    after(:all) { ActiveFedora::Base.delete_all }
+
+    it "returns a core file" do 
+      expect(@core.class).to eq CoreFile 
+    end
+
+    it "assigns a depositor to the core file" do 
+      expect(@core.depositor).to eq "tapasguy@brown.edu"
+    end
+
+    it "assigns a nid to the core file" do 
+      expect(@core.nid).to eq "123"
+    end
+
+    it "assigns the core file to the right parent collection" do 
+      expect(@core.collection.pid).to eq @collection.pid 
+    end
+
+    it "makes the core file private" do 
+      expect(@core.mass_permissions).to eq "private" 
+    end
+
+    it "instantiates the required tei_file object and makes it canonical" do 
+      expect(@core.canonical_object(:return_as => :models).class).to eq TEIFile
+    end
+
+    it "writes the expected content to the TEIFile object" do 
+      actual   = @core.canonical_object(:return_as => :models).content.content 
+      expected = File.read(@original_path)
+      expect(actual).to eq expected 
+    end
+
+    it "deletes the file at params[:file] from the filesystem." do 
+      expect(File.exists? @copy_path).to be false 
+    end
+  end
+
+  describe "Exception handling" do 
+
+  end
 end
