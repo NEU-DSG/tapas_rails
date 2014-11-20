0ed3c549e9eb6e186f167bc4daf2e2269b87db04
Will Jackson <wjackson64@gmail.com>
2014-11-20 18:24:49 +0000

    Update the validator to expect the four fields that the Drupal API actually passes.

--- app/validators/community_validator.rb
+++ app/validators/community_validator.rb
@@ -8,15 +8,15 @@ class CommunityValidator
   def validate_params
     return errors if no_params?
     validate_required_attributes
-    validate_parent
     return errors
   end
 
   def required_attributes
-    [:title, :parent]
-  end
-
-  def validate_parent
-    validate_parent_helper([Community])
+    case params[:action]
+    when "create"
+      [:nid, :title, :description, :users]
+    when "update"
+      #TODO
+    end
   end
 end
