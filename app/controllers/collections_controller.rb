87a14fb0a8be6f92d4eaa91a89edc78d6819463e
Will Jackson <wjackson64@gmail.com>
2014-11-24 17:56:16 +0000

    Add the collection controller #create action and associated smoke tests.

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,3 +1,10 @@
 class CollectionsController < ApplicationController
   include ApiAccessible
+
+  def create
+    job = TapasObjectCreationJob.new(params, "Collection")
+    TapasRails::Application::Queue.push(job)
+    @response[:message] = "Collection is being processed"
+    pretty_json(202) and return 
+  end
 end
--- spec/controllers/collections_controller_spec.rb
+++ spec/controllers/collections_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe CollectionsController do
+  let(:user) { FactoryGirl.create(:user) } 
+  let(:params) { { email: user.email, token: "test_api_key" } } 
+
   it_should_behave_like "an API enabled controller"
+
+  describe "POST #create" do 
+    after(:all) { ActiveFedora::Base.delete_all }
+
+    it "422s for invalid requests" do 
+      post :create, params
+      expect(response.status).to eq 422
+    end
+
+    it "returns a 202 and creates the requested collection on a valid request" do 
+      Resque.inline = true 
+      post_params = { title: "Collection", nid: "8018", project: "invalid" }
+      post_params = post_params.merge params
+      post :create, post_params
+
+      expect(response.status).to eq 202
+      expect(Collection.find_by_nid("8018")).not_to be nil 
+      Resque.inline = false
+    end
+  end
 end
