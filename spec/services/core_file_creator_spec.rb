278724057d0c890ea07ad42151e110dbf1ef5901
Will Jackson <wjackson64@gmail.com>
2014-12-04 19:43:36 +0000

    Clean up core file creator spec

--- spec/services/core_file_creator_spec.rb
+++ spec/services/core_file_creator_spec.rb
@@ -1,26 +1,20 @@
 require 'spec_helper'
 
 describe CoreFileCreator do 
-  def copy_tei
-    @original_path = "#{Rails.root}/spec/fixtures/files/tei.xml"
-    @copy_path     = "#{Rails.root}/spec/fixtures/files/tei_copy.xml"
-
-    FileUtils.cp(@original_path, @copy_path)
+  include FileHelpers
+  def params
+    { :depositor => "tapasguy@brown.edu", 
+      :nid => "123",
+      :collection => "3017",
+      :file => fixture_file("tei_copy.xml") }
   end
 
   describe "A clean run" do 
     before(:all) do 
-      copy_tei
-
-      params = {}
-      params[:depositor]     = "tapasguy@brown.edu"
-      params[:nid]       = "123"
-      params[:collection] = "3017"
-      params[:file]          = @copy_path
-
+      copy_fixture("tei.xml", "tei_copy.xml")
       # Instantiate the collection we expect to exist
       @collection = Collection.new
-      @collection.nid       = "3017"
+      @collection.nid = "3017"
       @collection.depositor = "tapasguy@brown.edu"
       @collection.save!
 
@@ -34,15 +28,15 @@ describe CoreFileCreator do
     end
 
     it "assigns a depositor to the core file" do 
-      expect(@core.depositor).to eq "tapasguy@brown.edu"
+      expect(@core.depositor).to eq params[:depositor]
     end
 
     it "assigns a nid to the core file" do 
-      expect(@core.nid).to eq "123"
+      expect(@core.nid).to eq params[:nid]
     end
 
     it "stores the nid of its parent as a distinct property" do 
-      expect(@core.og_reference).to eq "3017" 
+      expect(@core.og_reference).to eq params[:collection]
     end
     
     it "assigns the core file to the right parent collection" do 
@@ -59,24 +53,18 @@ describe CoreFileCreator do
 
     it "writes the expected content to the TEIFile object" do 
       actual   = @core.canonical_object(:return_as => :models).content.content 
-      expected = File.read(@original_path)
+      expected = File.read(fixture_file("tei.xml"))
       expect(actual).to eq expected 
     end
 
     it "deletes the file at params[:file] from the filesystem." do 
-      expect(File.exists? @copy_path).to be false 
+      expect(File.exists?(fixture_file("tei_copy.xml"))).to be false 
     end
   end
 
   describe "A run where the collection doesn't exist" do 
     before(:all) do
-      copy_tei  
-      params = {}
-      params[:depositor]     = "tapasguy@brown.edu"
-      params[:nid]       = "8873"
-      params[:collection] = "invalid"
-      params[:file]          = @copy_path
-      
+      copy_fixture("tei.xml", "tei_copy.xml")
       @core = CoreFileCreator.create_record(params)
     end
 
@@ -87,29 +75,22 @@ describe CoreFileCreator do
     end
 
     it "records the nid of its expected parent." do 
-      expect(@core.og_reference).to eq "invalid" 
+      expect(@core.og_reference).to eq params[:collection]
     end
   end
 
   describe "A run that errors out", :type => :mailer do 
-    before(:all) { copy_tei } 
+    before(:all) { copy_fixture("tei.xml", "tei_copy.xml") } 
     after(:all)  { ActiveFedora::Base.delete_all }
 
     it "persists no objects, deletes the TEI File, and triggers an exception notification." do
-      params = {
-        :depositor     => "tapasguy@brown.edu",
-        :nid       => "1919191",
-        :collection => "valid",
-        :file          => @copy_path,
-      }
-      
       # Ensure that file creation fails near the very end
       TEIFile.any_instance.stub(:save!).and_raise(RuntimeError)
 
       expect { CoreFileCreator.create_record(params) }.to raise_error(RuntimeError)
 
       expect(CoreFile.count).to eq 0
-      expect(File.exists? @copy_path).to be false
+      expect(File.exists?(fixture_file("tei_copy.xml"))).to be false
 
       # Validate the email
       expect(ActionMailer::Base.deliveries.length).to eq 1
