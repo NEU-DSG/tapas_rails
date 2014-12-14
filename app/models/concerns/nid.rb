3a4c8684dd8b9b51b74630209a576854860dc99c
Will Jackson <wjackson64@gmail.com>
2014-12-14 17:44:09 +0000

    Remove tapas_nid namespacing from mods datastream
    Given that 'fields from tapas' is the only thing that the repository is
    going to track for quite some time, namespacing out the nid field as
    tapas_nid in the datastream definition/solrization is sort of pointless
    + ugly + confusing, since the getter/setter methods are just defined as
    :nid and :nid=.  Changes everything over to nid.

--- app/models/concerns/nid.rb
+++ app/models/concerns/nid.rb
@@ -5,13 +5,13 @@ module Nid
     # Use solr to look up an object with a given content type 
     # by drupal node reference id.
     def self.find_by_nid(nid)
-      return self.where("tapas_nid_ssim" => nid).first
+      return self.where("nid_ssim" => nid).first
     end
 
     # Access fedora directly and check if an object with a particular 
     # nid already exists.
     def self.exists_by_nid?(nid)
-      ActiveFedora::Base.exists?("tapas_nid_ssim" => nid)
+      ActiveFedora::Base.exists?("nid_ssim" => nid)
     end
 
     has_attributes :nid, datastream: "mods", multiple: false 
--- app/models/datastreams/mods_datastream.rb
+++ app/models/datastreams/mods_datastream.rb
@@ -3,13 +3,13 @@ class ModsDatastream < CerberusCore::Datastreams::ModsDatastream
 
   extend_terminology do |t| 
     t.identifier(path: "identifier", namespace_prefix: "mods", attributes: { type: :none })
-    t.nid(path: "identifier", namespace_prefix: "mods", attributes: { type: "tapas_id" })
+    t.nid(path: "identifier", namespace_prefix: "mods", attributes: { type: "nid" })
   end
 
   def to_solr(solr_doc = {})
     solr_doc = super solr_doc 
 
-    solr_doc["tapas_nid_ssim"] = self.nid.first if self.nid.first.present?
+    solr_doc["nid_ssim"] = self.nid.first if self.nid.first.present?
 
     return solr_doc
   end
