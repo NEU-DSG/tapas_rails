497e258232dcc008446c42dd399f989554591e0b
Will Jackson <wjackson64@gmail.com>
2015-06-03 19:14:54 +0000

    Update core file controller for new upsert spec

--- app/controllers/core_files_controller.rb
+++ app/controllers/core_files_controller.rb
@@ -28,30 +28,16 @@ class CoreFilesController < ApplicationController
     # If params[:file] is set to anything, we assume 
     # we need to perform a file content update - extract 
     # filepath and filename
-    if params[:file].present?
-      params[:file] = http_file_upload_to_hash(params[:file])
-    end
-
-    if params[:support_files].present?
-      support_file_array = []
-      params[:support_files].map do |key, support_file|
-        support_file_array << http_file_upload_to_hash(support_file)
-      end
-
-      params[:support_files] = support_file_array
+    if params[:files].present?
+      fpath = params[:files].path
+      fname = params[:files].original_filename
+      tmp = Rails.root.join("tmp", "#{SecureRandom.hex}-#{fname}").to_s
+      FileUtils.mv(fpath, tmp)
+      params[:files] = tmp
     end
 
     TapasRails::Application::Queue.push TapasObjectUpsertJob.new params 
     @response[:message] = "CoreFile create/update in progress" 
     pretty_json(202) and return 
   end
-
-  private
-    def http_file_upload_to_hash(file_upload)
-      fpath = file_upload.path 
-      fname = file_upload.original_filename
-      tmp   = Rails.root.join("tmp", "fname#{SecureRandom.hex}").to_s
-      FileUtils.mv(fpath, tmp)
-      return { :name => fpath, :path => tmp }
-    end
 end
