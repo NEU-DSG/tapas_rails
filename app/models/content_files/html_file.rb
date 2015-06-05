a8ea0305ab399317753200ac6d202244766e0e3e
Will Jackson <wjackson64@gmail.com>
2015-06-05 14:32:06 +0000

    Add html_type= method to the HTMLFile class

--- app/models/content_files/html_file.rb
+++ app/models/content_files/html_file.rb
@@ -3,4 +3,19 @@ class HTMLFile < CerberusCore::BaseModels::ContentFile
 
   has_and_belongs_to_many :html_for, :property => :is_html_for, 
     :class_name => "CoreFile"
+
+  def html_type=(str)
+    unless %(teibp tapas_generic).include? str 
+      raise Exceptions::InvalidHTMLTypeError.new "HTML type must be one of: " \
+        "teibp, tapas_generic" 
+    end
+
+    properties.html_type = str 
+  end
+
+  def html_type 
+    properties.html_type.first
+  end
+
+  has_metadata :name => "properties", :type => PropertiesDatastream
 end
