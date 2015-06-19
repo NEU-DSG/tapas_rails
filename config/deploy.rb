72e4ffa4b4972785dbcc6ab17fdb82c7311a2346
Will Jackson <wjackson64@gmail.com>
2015-06-19 20:40:35 +0000

    Fix typo

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execut "touch #{release_path}/tmp/restart.txt"
+      execute "touch #{release_path}/tmp/restart.txt"
       execute "service tomcat restart"
     end
   end
