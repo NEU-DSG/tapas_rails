d385f2ad3e278ffe3b13c4ff1603e642d793636e
Will Jackson <wjackson64@gmail.com>
2015-06-23 15:47:02 +0000

    Adds a #any_public_collection? method to SolrDocs
    This can be used to quickly determine whether a CoreFile should have its
    drupal_access level set to private when a given Collection changes its
    permission level.

---
+++ spec/models/solr_document_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper' 
+
+describe SolrDocument do 
+  describe '#any_public_collections?' do 
+    
+    it 'returns false when called on non-CoreFiles' do 
+      doc = SolrDocument.new FactoryGirl.create(:collection).to_solr 
+
+      expect(doc.any_public_collections?).to be false
+    end
+
+    it 'returns false for CoreFiles with no collections' do 
+      doc = SolrDocument.new FactoryGirl.create(:core_file).to_solr 
+
+      expect(doc.any_public_collections?).to be false 
+    end
+
+    it 'returns false for CoreFiles with all private collections' do 
+      cf = FactoryGirl.create :core_file 
+      coll1, coll2 = FactoryGirl.create_list(:collection, 2)
+
+      coll1.drupal_access = 'private' ; coll1.save! 
+      coll2.drupal_access = 'private' ; coll2.save!
+
+      cf.collections = [coll1, coll2] ; cf.save! 
+
+      doc = SolrDocument.new cf.to_solr 
+
+      expect(doc.any_public_collections?).to be false 
+    end
+
+    it 'returns true for CoreFiles with a public collection' do 
+      cf = FactoryGirl.create :core_file 
+      coll1, coll2 = FactoryGirl.create_list(:collection, 2) 
+
+      coll1.drupal_access = 'private' ; coll1.save! 
+      coll2.drupal_access = 'public' ; coll2.save! 
+
+      cf.collections = [coll1, coll2] ; cf.save!
+
+      doc = SolrDocument.new cf.to_solr 
+
+      expect(doc.any_public_collections?).to be true
+    end
+  end
+end
