2cde5079d1cc859d8d8178517d9a64991a2c0e0b
Will Jackson <wjackson64@gmail.com>
2015-07-10 18:03:19 +0000

    Add ExistError

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -2,4 +2,5 @@ module Exceptions
   class InvalidZipError < StandardError ; end 
   class InvalidHTMLTypeError < StandardError ; end
   class QueryObjectError < StandardError ; end
+  class ExistError < StandardError ; end
 end
