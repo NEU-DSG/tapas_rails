bb9e0435b711ef05b755ca0deec6728eb16f2a96
Will Jackson <wjackson64@gmail.com>
2015-07-15 20:32:57 +0000

    Make collections define their own #delete action
    api_accessible_spec now sends a well formed post#upsert request.  Also
    cleans up collections controller spec.

--- spec/shared/api_accessible_spec.rb
+++ spec/shared/api_accessible_spec.rb
@@ -4,16 +4,15 @@ shared_examples_for "an API enabled controller" do
   let(:user) { FactoryGirl.create(:user) } 
 
   describe "authentication" do 
-
     it "raises a 403 for requests with no authorization header" do
       set_auth_token(nil)
-      post :upsert
+      post :upsert, :did => 'whatever'
       expect(response.status).to eq 403
     end
 
     it "raises a 403 for requests with an invalid token" do 
       set_auth_token('bupkes')
-      post :upsert
+      post :upsert, :did => 'whatever'
       expect(response.status).to eq 403
     end
   end
