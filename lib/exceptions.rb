93fd598cdf8b68e5f39d931d89ef823e3a574706
Will Jackson <wjackson64@gmail.com>
2015-07-15 17:00:53 +0000

    Add duplicate Drupal ID error to project

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -2,4 +2,5 @@ module Exceptions
   class InvalidZipError < StandardError ; end 
   class InvalidHTMLTypeError < StandardError ; end
   class QueryObjectError < StandardError ; end
+  class DuplicateDidError < StandardError ; end
 end
