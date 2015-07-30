b1074089038ae5d10fd9fdedd74417ae51d3bfd1
Will Jackson <wjackson64@gmail.com>
2015-07-30 19:35:38 +0000

    Updates #all_ography_tei_files to expect CoreFile
    Previously, this expected to be called on a Community (Project) object.
    However, since we want to attach ographies to one or more Collections
    instead (i.e. normal TEI content handling) it makes more sense to expect
    this to be called on a CoreFile that can then be queried for its
    containing collections.

--- spec/models/concerns/tapas_queries_spec.rb
+++ spec/models/concerns/tapas_queries_spec.rb
@@ -6,9 +6,9 @@ describe 'TapasQueries' do
 
     after(:all) { ActiveFedora::Base.delete_all }
 
-    def create_ography(community, ography_assignment) 
-      core_file = FactoryGirl.create :core_file 
-      core_file.send(ography_assignment, [community])
+    def create_ography(collection, ography_assignment) 
+      core_file = FactoryGirl.create(:core_file)
+      core_file.send(ography_assignment, [collection])
       core_file.save! 
 
       tei = TEIFile.create(:depositor => 'system') 
@@ -21,27 +21,32 @@ describe 'TapasQueries' do
 
     # This is sort of a monster of a single spec item but I'm not sure how 
     # to fix it or if it matters.
-    it "can retrieve all ography tei files associated with a community" do 
-      community = FactoryGirl.create :community 
+    it "can retrieve all ography tei files associated with a CoreFile's"\
+       " Collections." do
+      collection1 = FactoryGirl.create :collection
+      collection2 = FactoryGirl.create :collection 
+      collection3 = FactoryGirl.create :collection
 
-      otherography = create_ography(community, :otherography_for=)
-      personography = create_ography(community, :personography_for=)
-      orgography = create_ography(community, :orgography_for=)
-      bibliography = create_ography(community, :bibliography_for=)
-      odd_file = create_ography(community, :odd_file_for=)      
+      otherography = create_ography(collection1, :otherography_for=)
+      personography = create_ography(collection2, :personography_for=)
+      orgography = create_ography(collection3, :orgography_for=)
+      bibliography = create_ography(collection1, :bibliography_for=)
+      odd_file = create_ography(collection2, :odd_file_for=)      
 
-      community.reload 
+      core_file = FactoryGirl.create :core_file
+      core_file.collections = [collection1, collection2, collection3]
+      core_file.save!
 
       expected_pids = [otherography.pid, personography.pid, orgography.pid,
        bibliography.pid, odd_file.pid] 
 
-      # Assert that this works for community objects
-      result = community.all_ography_tei_files(:solr_docs) 
+      # Assert that this works for CoreFile objects
+      result = core_file.all_ography_tei_files(:solr_docs) 
       result.map! { |x| x.pid }
       expect(result).to match_array expected_pids
 
       # Assert that this works for SolrDocument objects
-      solr_doc = SolrDocument.new community.to_solr 
+      solr_doc = SolrDocument.new core_file.to_solr 
       result = solr_doc.all_ography_tei_files(:solr_docs) 
       result.map! { |x| x.pid } 
       expect(result).to match_array expected_pids
