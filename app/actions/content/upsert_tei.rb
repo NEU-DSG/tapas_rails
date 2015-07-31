23338ee5f16a98619425d70702c0bd932be82573
Will Jackson <wjackson64@gmail.com>
2015-07-31 16:09:41 +0000

    Update Tei Content create/update.

---
+++ app/actions/content/upsert_tei.rb
@@ -0,0 +1,36 @@
+module Content
+  class UpsertTei
+    attr_reader :core_file
+    attr_reader :file
+
+    include Content
+
+    def initialize(core_file, file)
+      @core_file = core_file
+      @file = file 
+    end
+
+    def self.execute(core_file, file)
+      self.new(core_file, file).upsert!
+    end
+
+    def upsert! 
+      begin
+        ZipContentValidator.tei file
+
+        content = core_file.canonical_object
+
+        unless content 
+          content = TEIFile.create
+          content.canonize
+          content.core_file = core_file
+          content.save! 
+        end
+
+        add_unique_file(content, file)
+      ensure
+        FileUtils.rm_f file
+      end
+    end
+  end
+end
