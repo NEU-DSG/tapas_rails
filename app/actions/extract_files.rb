fc7213ffca8d6c61947763310817851a8fb77a95
Will Jackson <wjackson64@gmail.com>
2015-08-03 17:40:45 +0000

    Update extract files action to reflect new zip structure

--- app/actions/extract_files.rb
+++ app/actions/extract_files.rb
@@ -10,8 +10,8 @@ class ExtractFiles
     @zip_path = zip_path 
   end
 
-  def self.extract!(zip_path)
-    ExtractFiles.new(zip_path).extract!
+  def self.execute(zip_path)
+    ExtractFiles.new(zip_path).execute
   end
 
   # Returns a hash with each of the following keys set to a file path assuming
@@ -22,39 +22,27 @@ class ExtractFiles
   # Also returns the following key set to an array of files paths: 
   # - support_files: All support files (at this point, this basically means 
   # images) associated with this upload
-  def extract!
-    begin 
-      response = {} 
-      @tmp_dir = "#{Rails.root}/tmp/extracted_files/#{SecureRandom.hex}"
-      FileUtils.mkdir_p tmp_dir
+  def execute
+    response = {} 
+    @tmp_dir = "#{Rails.root}/tmp/extracted_files/#{SecureRandom.hex}"
+    FileUtils.mkdir_p tmp_dir
 
-      response[:directory] = @tmp_dir
+    response[:directory] = @tmp_dir
 
-      # Ensure support_files is always initialized to an empty array
-      response[:support_files] = []
+    # Ensure support_files is always initialized to an empty array
+    response[:support_files] = []
 
-      Zip::File.open(zip_path) do |zip|
-        zip.each do |entry| 
-          name = entry.name
-          if Pathname.new(name).basename.to_s == "mods.xml"
-            response[:mods] = write_to_file entry 
-          elsif Pathname.new(name).basename.to_s == "tfc.xml" 
-            response[:tfc] = write_to_file entry 
-          elsif name.split("/").second == "tei" && real_file?(entry)
-            response[:tei] = write_to_file entry
-          elsif name.split("/").second == "teibp" && real_file?(entry)
-            response[:teibp] = write_to_file entry 
-          elsif name.split("/").second == "tapas_generic" && real_file?(entry)
-            response[:tapas_generic] = write_to_file entry
-          elsif name.split("/").second == "support_files" && real_file?(entry)
-            response[:support_files] << write_to_file(entry)
-          end
+    Zip::File.open(zip_path) do |zip|
+      zip.each do |entry| 
+        name = entry.name
+        if name.split('/').second == 'thumbnail' && real_file?(entry)
+          response[:thumbnail] = write_to_file entry
+        elsif name.split("/").second == "support_files" && real_file?(entry)
+          response[:support_files] << write_to_file(entry)
         end
       end
-      response
-    ensure
-      FileUtils.rm zip_path if File.exists? zip_path
     end
+    response
   end
 
   private 
--- spec/actions/extract_files_spec.rb
+++ spec/actions/extract_files_spec.rb
@@ -3,40 +3,15 @@ require "spec_helper"
 describe ExtractFiles do 
   include FileHelpers
 
-  describe "#extract!" do 
+  describe "#execute" do 
     context "with all supported file types" do 
       before(:all) do 
-        copy_fixture("all_files.zip", "all_files_copy.zip")
-        @response = ExtractFiles.extract!(fixture_file("all_files_copy.zip"))
+        @response = ExtractFiles.execute(fixture_file("all_files.zip"))
       end
 
-      #Cleanup
       after(:all) do 
         FileUtils.rm_r("#{Rails.root}/tmp/extracted_files")
       end  
-
-      it "creates a file holding teibp content" do 
-        expect(File.exists?(@response[:teibp])).to be true 
-        expect(File.size(@response[:teibp])).not_to eq 0 
-      end
-
-      it "creates a file holding tapas_generic content" do 
-        expect(File.exists?(@response[:tapas_generic])).to be true 
-        expect(File.size(@response[:tapas_generic])).not_to eq 0 
-      end
-
-      it "creates a file holding mods content" do 
-        expect(File.exists?(@response[:mods])).to be true 
-        expect(File.size(@response[:mods])).not_to eq 0
-      end
-
-      it "creates a file holding tei content" do 
-        expect(File.exists?(@response[:tei])).to be true 
-      end
-
-      it "creates a file holding tfc content" do 
-        expect(File.exists?(@response[:tfc])).to be true 
-      end
 
       it "creates multiple files holding support image content" do 
         @response[:support_files].each do |sf| 
@@ -44,47 +19,19 @@ describe ExtractFiles do
         end 
       end
 
-      it "removes the zipfile when it completes" do 
-        expect(File.exists?("all_files_copy.zip")).to be false 
+      it 'creates a file holding thumbnail content' do 
+        thumbnail = @response[:thumbnail]
+
+        expect(thumbnail).not_to be nil 
+        expect(File.exists? thumbnail).to be true
+
+        filename = Pathname.new(thumbnail).basename.to_s
+        expect(filename).to eq 'thumbnail.jpg'
       end
 
       it "stores the directory that the files were written to" do 
         expect(@response[:directory]).to include "#{Rails.root}/tmp/extracted_files"
-        expect(@response[:tei]).to include @response[:directory]
-      end
-    end
-
-    context "with no support files or mods record" do 
-      before(:all) do 
-        copy_fixture("tei_tfc_only.zip", "tei_tfc_only_copy.zip")
-        @response = ExtractFiles.extract!(fixture_file("tei_tfc_only_copy.zip"))
-      end
-
-      after(:all) { FileUtils.rm_r "#{Rails.root}/tmp/extracted_files" }
-
-      it "returns nil for the mods file" do 
-        expect(@response[:mods]).to be nil 
-      end
-
-      it "returns nil for the html file" do 
-        expect(@response[:html]).to be nil 
-      end
-
-      it "returns an empty array for the support files array" do 
-        expect(@response[:support_files]).to eq []
-      end
-
-      it "returns a file holding tfc content" do 
-        expect(File.exists?(@response[:tfc])).to be true 
-      end
-
-      it 'returns a file holding tei content' do 
-        expect(File.exists?(@response[:tei])).to be true
-      end
-
-      it "stores the directory that the files were written to" do 
-        expect(@response[:directory]).to include "#{Rails.root}/tmp/extracted_files"
-        expect(@response[:tei]).to include @response[:directory]
+        expect(@response[:thumbnail]).to include @response[:directory]
       end
     end
   end
