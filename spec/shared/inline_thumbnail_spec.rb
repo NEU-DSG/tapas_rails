045bcf4e91f06e6a6091e2573043eab45f6b71f5
Will Jackson <wjackson64@gmail.com>
2015-08-06 19:12:44 +0000

    Use binread to get fixture file blobs

--- spec/shared/inline_thumbnail_spec.rb
+++ spec/shared/inline_thumbnail_spec.rb
@@ -12,13 +12,13 @@ shared_examples_for 'InlineThumbnails' do
 
   it 'can add an object by filepath' do 
     object.add_thumbnail(:filepath => thumb)
-    expect(object.thumbnail_1.content).to eq File.read(thumb)
+    expect(object.thumbnail_1.content).to eq IO.binread(thumb)
     expect(object.thumbnail_1.label).to eq 'image.jpg'
     expect(object.thumbnail_list).to eq [object.download_path('thumbnail_1')]
   end
 
   it 'can add an object by name and blob' do 
-    blob = File.read(thumb)
+    blob = IO.binread(thumb)
     object.add_thumbnail(:name => 'a.png', :content => blob)
     expect(object.thumbnail_1.content).to eq blob
     expect(object.thumbnail_1.label).to eq 'a.png'
