3eb220175317fede4702cbd7016acc5b0a0f2830
Will Jackson <wjackson64@gmail.com>
2015-08-24 16:05:49 +0000

    Make copy_fixture_file write to the Rails tmp dir
    This allows us to update upsert actions to check that files are in a tmp
    directory before deleting them.  We also add a #tmp_fixture_file method
    to get the path of files copied into the tmp directory.

--- spec/support/file_helpers.rb
+++ spec/support/file_helpers.rb
@@ -3,9 +3,13 @@ module FileHelpers
     return Rails.root.join("spec/fixtures/files", fname).to_s
   end
 
+  def tmp_fixture_file(fname)
+    return Rails.root.join("tmp", fname).to_s
+  end
+
   def copy_fixture(src_file, dest_file)
     src = Rails.root.join("spec/fixtures/files", src_file)
-    dest = Rails.root.join("spec/fixtures/files", dest_file)
+    dest = Rails.root.join("tmp/", dest_file)
 
     FileUtils.cp(src.to_s, dest.to_s)
     return dest.to_s
