66c14611912b19be898749e9f9747ba43e37d68a
Will Jackson <wjackson64@gmail.com>
2015-10-02 21:19:59 +0000

    Rename ValidatorHelpers to Validations
    This avoids potential confusion with the ValidatorHelpers module present
    in the test suite.

--- app/validators/concerns/validations.rb
+++ app/validators/concerns/validations.rb
@@ -1,7 +1,9 @@
-module ValidatorHelpers
+module Validations
+  extend ActiveSupport::Concern
+
   def validate_did_and_create_reqs(klass, required_params)
     did_in_use = Did.exists_by_did?(params[:did])
-    right_class = klass.find_by_did(params[:did])
+    right_class = klass.exists_by_did?(params[:did])
 
     if did_in_use && !right_class
       self.errors << "Attempted to reuse a drupal id" 
@@ -15,8 +17,6 @@ module ValidatorHelpers
   end
 
   def validate_all_present_params
-    puts self
-    puts params
     params.each do |param_name, param_value|
       if self.respond_to? :"validate_#{param_name}"
         self.send(:"validate_#{param_name}")
@@ -31,9 +31,11 @@ module ValidatorHelpers
   end
 
   def validate_file_and_type(param, valid_extensions)
-    if !(params[param].is_a? ActionDispatch::Http::UploadedFile)
+    valid_classes = [ActionDispatch::Http::UploadedFile, Rack::Test::UploadedFile]
+    param_class = params[param].class
+    if !(param_class.in? valid_classes)
       self.errors << "#{param} must be a file upload" 
-    elsif params[param].original_filename.split('.').last.in? valid_extensions
+    elsif !(params[param].original_filename.split('.').last.in? valid_extensions)
       all_valid_extensions = valid_extensions.join(' or ')
       errors << "#{param} must be file with extension: #{all_valid_extensions}"
     end
