d9dcdf742a4db276b0cf0b2ee8823ee494874587
Will Jackson <wjackson64@gmail.com>
2015-10-06 20:35:49 +0000

    Update community validator spec

--- spec/validators/community_validator_spec.rb
+++ spec/validators/community_validator_spec.rb
@@ -3,65 +3,62 @@ require 'spec_helper'
 describe CommunityValidator do 
   include ValidatorHelpers
 
-  describe '#validate_required_attributes' do 
+  def validate_attributes(params)
+    validator = CommunityValidator.new params
+    validator.validate_upsert
+  end
 
-    def validate_attributes(params)
-      validator = CommunityValidator.new params
-      validator.validate_upsert
+  context 'on create' do 
+    let(:params) do 
+      { :members => %w(peter paul mary),
+        :did => SecureRandom.uuid,
+        :depositor => 'bjork', 
+        :access => 'public', 
+        :title => 'A Sample Project', 
+        :description => 'A sample project.', }
     end
 
-    context 'on create' do 
-      let(:params) do 
-        { :members => %w(peter paul mary),
-          :did => SecureRandom.uuid,
-          :depositor => 'bjork', 
-          :access => 'public', 
-          :title => 'A Sample Project', 
-          :description => 'A sample project.', }
-      end
-
-      it 'requires members' do 
-        errors = validate_attributes(params.except(:members))
-        expect(errors.length).to eq 1 
-        expect(errors.first).to include 'members'
-      end
-
-      it 'requires a depositor' do 
-        errors = validate_attributes(params.except(:depositor))
-        expect(errors.length).to eq 1
-        expect(errors.first).to include 'depositor'
-      end
-
-      it 'requires an access level' do 
-        errors = validate_attributes(params.except(:access))
-        expect(errors.length).to eq 1 
-        expect(errors.first).to include 'access'
-      end
-
-      it 'requires a title' do 
-        errors = validate_attributes(params.except(:title))
-        expect(errors.length).to eq 1 
-        expect(errors.first).to include 'title' 
-      end
-
-      it 'requires a description' do 
-        errors = validate_attributes(params.except(:description))
-        expect(errors.length).to eq 1 
-        expect(errors.first).to include 'description'
-      end
+    it 'requires members' do 
+      errors = validate_attributes(params.except(:members))
+      expect(errors.length).to eq 1 
+      expect(errors.first).to include 'members'
     end
 
-    context 'on update' do 
-      before(:all) do 
-        @community = FactoryGirl.create :community
-      end
+    it 'requires a depositor' do 
+      errors = validate_attributes(params.except(:depositor))
+      expect(errors.length).to eq 1
+      expect(errors.first).to include 'depositor'
+    end
 
-      after(:all) { @community.destroy }
+    it 'requires an access level' do 
+      errors = validate_attributes(params.except(:access))
+      expect(errors.length).to eq 1 
+      expect(errors.first).to include 'access'
+    end
 
-      it 'requires no params' do 
-        errors = validate_attributes({:did => @community.did})
-        expect(errors.length).to eq 0
-      end
+    it 'requires a title' do 
+      errors = validate_attributes(params.except(:title))
+      expect(errors.length).to eq 1 
+      expect(errors.first).to include 'title' 
+    end
+
+    it 'requires a description' do 
+      errors = validate_attributes(params.except(:description))
+      expect(errors.length).to eq 1 
+      expect(errors.first).to include 'description'
+    end
+  end
+
+  context 'on update' do 
+    before(:all) do 
+      @community = FactoryGirl.create :community
+    end
+
+    after(:all) { @community.destroy }
+
+    it 'requires no params' do 
+      errors = validate_attributes({:did => @community.did})
+      expect(errors.length).to eq 0
     end
   end
 end
