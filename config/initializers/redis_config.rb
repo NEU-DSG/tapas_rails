180a25149d5dd97e80e0e96fa884861fc61cbd11
Eli Zoller <zoller.eli@gmail.com>
2016-10-20 13:47:02 -0400

    trying to get redis to play nice

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -9,7 +9,8 @@ if defined?(PhusionPassenger)
       # The important two lines
       $redis.client.disconnect if $redis
       $redis = Redis.new(host: config[:host], port: config[:port], thread_safe: true) rescue nil
-      Resque.redis.client.reconnect if Resque.redis
+      $redis.client.reconnect
+      # Resque.redis.client.reconnect if Resque.redis
     end
   end
 else
