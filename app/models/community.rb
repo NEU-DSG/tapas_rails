b94b6bf0d3ed05b942c7c24ca710f67d77588ec1
Eli Zoller <zoller.eli@gmail.com>
2016-10-25 15:19:31 -0400

    mapping DC to Mods

--- app/models/community.rb
+++ app/models/community.rb
@@ -5,18 +5,21 @@ class Community < CerberusCore::BaseModels::Community
   include TapasQueries
   include InlineThumbnail
   include StatusTracking
+  include TapasRails::MetadataAssignment
 
   before_save :ensure_unique_did
 
   has_collection_types ["Collection"]
   has_community_types  ["Community"]
 
-  parent_community_relationship :community 
+  parent_community_relationship :community
 
   has_metadata :name => "mods", :type => ModsDatastream
   has_metadata :name => "properties", :type => PropertiesDatastream
 
   has_attributes :project_members, datastream: "properties", multiple: true
+  has_attributes :title, datastream: "mods"
+  has_attributes :description, datastream: "DC"
 
   # Look up or create the root community of the graph
   def self.root_community
@@ -26,7 +29,7 @@ class Community < CerberusCore::BaseModels::Community
       community = Community.new(:pid => Rails.configuration.tap_root)
       community.depositor = "000000000"
       community.mods.title = "TAPAS root"
-      community.mass_permissions = "private" 
+      community.mass_permissions = "private"
       community.save!
       return community
     end
@@ -34,12 +37,17 @@ class Community < CerberusCore::BaseModels::Community
 
   def as_json
     fname = (thumbnail_1.label == 'File Datastream' ? '' : thumbnail_1.label)
-    { :members => project_members, 
-      :depositor => depositor, 
-      :access => drupal_access, 
-      :thumbnail => fname, 
-      :title => mods.title.first, 
+    { :members => project_members,
+      :depositor => depositor,
+      :access => drupal_access,
+      :thumbnail => fname,
+      :title => mods.title.first,
       :description => mods.abstract.first
     }
+  end
+
+  def match_dc_to_mods
+    self.DC.title = self.mods.title.first
+    self.DC.description = self.mods.abstract.first if !self.mods.abstract.blank?
   end
 end
