f06c463543419faad6a2b47c623fd8abb9c01372
Eli Zoller <zoller.eli@gmail.com>
2017-02-15 10:42:24 -0500

    separate helper methods into dir_name and machine_name

--- lib/tapas_rails/view_packages.rb
+++ lib/tapas_rails/view_packages.rb
@@ -1,8 +1,14 @@
 module TapasRails
   module ViewPackages
-    def available_view_packages
-      Rails.cache.fetch("view_packages", expires_in: 48.hours) do
+    def available_view_packages_machine
+      Rails.cache.fetch("view_packages_machine", expires_in: 48.hours) do
         ViewPackage.where("").pluck(:machine_name).to_a
+      end
+    end
+
+    def available_view_packages_dir
+      Rails.cache.fetch("view_packages_dir", expires_in: 48.hours) do
+        ViewPackage.where("").pluck(:dir_name).to_a
       end
     end
   end
