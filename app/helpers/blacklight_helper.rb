88e6da484435b85df1a826f12b81a5821ef9cb2d
Eli Zoller <zoller.eli@gmail.com>
2017-02-22 07:37:21 -0500

    add routes based on object model in catalog

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -43,4 +43,28 @@ module BlacklightHelper
     end
     nil
   end
+
+  ##
+  # Extension point for downstream applications
+  # to provide more interesting routing to
+  # documents
+  def url_for_document doc, options = {}
+    logger.error "in url_for_document"
+    if respond_to?(:blacklight_config) and
+        blacklight_config.show.route and
+        (!doc.respond_to?(:to_model) or doc.to_model.is_a? SolrDocument)
+      route = blacklight_config.show.route.merge(action: :show, id: doc).merge(options)
+      route[:controller] = controller_name if route[:controller] == :current
+      route
+    else
+      if doc['active_fedora_model_ssi']
+        klass = doc['active_fedora_model_ssi'].to_s.pluralize.underscore
+      else
+        klass = "catalog"
+      end
+
+      {controller:klass, id: doc.id, action: "show"}
+    end
+  end
+
 end
