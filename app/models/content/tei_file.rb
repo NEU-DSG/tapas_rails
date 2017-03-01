a6b8fb450da57e16fd54989815d30b11414f39b7
Eli Zoller <zoller.eli@gmail.com>
2017-03-01 08:43:14 -0500

    adding fedora_file_path method for tei_file objects

--- app/models/content/tei_file.rb
+++ app/models/content/tei_file.rb
@@ -1,9 +1,19 @@
-class TEIFile < CerberusCore::BaseModels::ContentFile 
+class TEIFile < CerberusCore::BaseModels::ContentFile
   include Filename
   include DownloadPath
 
   core_file_relationship :core_file
 
-  has_and_belongs_to_many :tfc_for, :property => :is_tfc_for, 
-    :class_name => "CoreFile" 
+  has_and_belongs_to_many :tfc_for, :property => :is_tfc_for,
+    :class_name => "CoreFile"
+
+  def fedora_file_path
+    config_path = Rails.application.config.fedora_home
+    datastream_str = "info:fedora/#{self.pid}/content/content.0"
+    escaped_datastream = Rack::Utils.escape(datastream_str)
+    md5_str = Digest::MD5.hexdigest(datastream_str)
+    dir_name = md5_str[0,2]
+    file_path = config_path + dir_name + "/" + escaped_datastream
+    return file_path
+  end
 end
