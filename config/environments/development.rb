6a12c5814dbc8397257652811316f9ffefc35188
Eli Zoller <zoller.eli@gmail.com>
2017-03-01 08:45:27 -0500

    adding fedora_home paths to environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,8 @@ TapasRails::Application.configure do
 
   config.log_level = :debug
 
+  config.fedora_home = "#{Rails.root.to_s}/jetty/fedora/default/data/datastreamStore/"
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,25 +1,26 @@
-TapasRails::Application.configure do 
+TapasRails::Application.configure do
 
-  config.cache_classes = true 
+  config.cache_classes = true
+  config.fedora_home = "/opt/fedora/data/datastreamStore/"
 
-  config.eager_load = true 
+  config.eager_load = true
 
-  config.consider_all_requests_local       = false 
-  config.action_controller.perform_caching = true 
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
 
-  config.assets.js_compressor = :uglifier 
+  config.assets.js_compressor = :uglifier
 
   config.assets.compile = true
 
-  config.assets.digest = true 
+  config.assets.digest = true
 
   config.assets.version = "1.0"
 
   config.log_level = :debug
 
-  config.i18n.fallbacks = true 
+  config.i18n.fallbacks = true
 
-  config.active_support.deprecation = :notify 
+  config.active_support.deprecation = :notify
 
-  config.log_formatter = ::Logger::Formatter.new 
+  config.log_formatter = ::Logger::Formatter.new
 end
