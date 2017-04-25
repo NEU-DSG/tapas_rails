4742db469be1f9b906d91bbe3f0afda69b7f5767
Eli Zoller <zoller.eli@gmail.com>
2017-04-25 14:15:14 -0400

    allow bootsy for pages and validate uniqueness of slug

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,9 @@
 class Page < ActiveRecord::Base
   extend FriendlyId
+  include Bootsy::Container
+
   attr_accessible :title, :content, :slug if Rails::VERSION::MAJOR < 4
   validates_presence_of :title, :content, :slug
+  validates :slug, uniqueness: { case_sensitive: false }
   friendly_id :slug, use: :slugged
 end
