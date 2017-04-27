97a1d98b26da1f80e15b5165d30cc72bcde5fc07
Eli Zoller <zoller.eli@gmail.com>
2017-04-27 09:24:56 -0400

    forem generator migrations and adding name field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425151606) do
+ActiveRecord::Schema.define(version: 20170426175101) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",       null: false
@@ -38,6 +38,103 @@ ActiveRecord::Schema.define(version: 20170425151606) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "forem_categories", force: true do |t|
+    t.string   "name",                   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "slug"
+    t.integer  "position",   default: 0
+  end
+
+  add_index "forem_categories", ["slug"], name: "index_forem_categories_on_slug", unique: true
+
+  create_table "forem_forums", force: true do |t|
+    t.string  "name"
+    t.text    "description"
+    t.integer "category_id"
+    t.integer "views_count", default: 0
+    t.string  "slug"
+    t.integer "position",    default: 0
+  end
+
+  add_index "forem_forums", ["slug"], name: "index_forem_forums_on_slug", unique: true
+
+  create_table "forem_groups", force: true do |t|
+    t.string "name"
+  end
+
+  add_index "forem_groups", ["name"], name: "index_forem_groups_on_name"
+
+  create_table "forem_memberships", force: true do |t|
+    t.integer "group_id"
+    t.integer "member_id"
+  end
+
+  add_index "forem_memberships", ["group_id"], name: "index_forem_memberships_on_group_id"
+
+  create_table "forem_moderator_groups", force: true do |t|
+    t.integer "forum_id"
+    t.integer "group_id"
+  end
+
+  add_index "forem_moderator_groups", ["forum_id"], name: "index_forem_moderator_groups_on_forum_id"
+
+  create_table "forem_posts", force: true do |t|
+    t.integer  "topic_id"
+    t.text     "text"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "reply_to_id"
+    t.string   "state",       default: "pending_review"
+    t.boolean  "notified",    default: false
+  end
+
+  add_index "forem_posts", ["reply_to_id"], name: "index_forem_posts_on_reply_to_id"
+  add_index "forem_posts", ["state"], name: "index_forem_posts_on_state"
+  add_index "forem_posts", ["topic_id"], name: "index_forem_posts_on_topic_id"
+  add_index "forem_posts", ["user_id"], name: "index_forem_posts_on_user_id"
+
+  create_table "forem_subscriptions", force: true do |t|
+    t.integer "subscriber_id"
+    t.integer "topic_id"
+  end
+
+  create_table "forem_topics", force: true do |t|
+    t.integer  "forum_id"
+    t.integer  "user_id"
+    t.string   "subject"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "locked",       default: false,            null: false
+    t.boolean  "pinned",       default: false
+    t.boolean  "hidden",       default: false
+    t.datetime "last_post_at"
+    t.string   "state",        default: "pending_review"
+    t.integer  "views_count",  default: 0
+    t.string   "slug"
+  end
+
+  add_index "forem_topics", ["forum_id"], name: "index_forem_topics_on_forum_id"
+  add_index "forem_topics", ["slug"], name: "index_forem_topics_on_slug", unique: true
+  add_index "forem_topics", ["state"], name: "index_forem_topics_on_state"
+  add_index "forem_topics", ["user_id"], name: "index_forem_topics_on_user_id"
+
+  create_table "forem_views", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "viewable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "count",             default: 0
+    t.string   "viewable_type"
+    t.datetime "current_viewed_at"
+    t.datetime "past_viewed_at"
+  end
+
+  add_index "forem_views", ["updated_at"], name: "index_forem_views_on_updated_at"
+  add_index "forem_views", ["user_id"], name: "index_forem_views_on_user_id"
+  add_index "forem_views", ["viewable_id"], name: "index_forem_views_on_viewable_id"
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
@@ -69,12 +166,12 @@ ActiveRecord::Schema.define(version: 20170425151606) do
   add_index "searches", ["user_id"], name: "index_searches_on_user_id"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "",               null: false
+    t.string   "encrypted_password",     default: "",               null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,                null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -84,6 +181,10 @@ ActiveRecord::Schema.define(version: 20170425151606) do
     t.boolean  "guest",                  default: false
     t.string   "encrypted_api_key"
     t.string   "role"
+    t.boolean  "forem_admin",            default: false
+    t.string   "forem_state",            default: "pending_review"
+    t.boolean  "forem_auto_subscribe",   default: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
