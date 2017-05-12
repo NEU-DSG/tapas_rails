9f1c1760579dc157bf717c280edd654e0a388d8d
Eli Zoller <zoller.eli@gmail.com>
2017-05-12 14:05:29 -0400

    fix for .public? method on solr_doc for can_read? methods

--- lib/tapas_rails/solr_document_behavior.rb
+++ lib/tapas_rails/solr_document_behavior.rb
@@ -3,5 +3,13 @@ module TapasRails
     def path
       send("Rails.application.routes.url_helpers.#{self.klass.underscore}_path", self.pid)
     end
+
+    def public?
+      read_groups.include?('public')
+    end
+
+    def read_groups
+      Array(self[Ability.read_group_field])
+    end
   end
 end
