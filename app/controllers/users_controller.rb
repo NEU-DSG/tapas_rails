69835f20603138a7e25344b7e0aa5cd7a4ac8da7
Eli Zoller <zoller.eli@gmail.com>
2017-05-26 10:37:21 -0400

    adding a before_filter for if the user is logged in on the my_tapas methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < CatalogController
 
   self.copy_blacklight_config_from(CatalogController)
+  before_filter :check_for_logged_in_user, :only => [:my_tapas, :my_projects]
 
   def my_tapas
     @page_title = "My TAPAS"
@@ -18,16 +19,24 @@ class UsersController < CatalogController
     render 'my_projects'
   end
 
-  def my_communities_filter(solr_parameters, user_parameters)
-    model_type = RSolr.solr_escape "info:fedora/afmodel:Community"
-    query = "has_model_ssim:\"#{model_type}\" && project_members_ssim:\"#{@user.id}\""
-    solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << query
-  end
-
   def search_action_url(options = {})
     # Rails 4.2 deprecated url helpers accepting string keys for 'controller' or 'action'
     catalog_index_path(options.except(:controller, :action))
   end
+
+  private
+    def my_communities_filter(solr_parameters, user_parameters)
+      model_type = RSolr.solr_escape "info:fedora/afmodel:Community"
+      query = "has_model_ssim:\"#{model_type}\" && (project_members_ssim:\"#{@user.id}\" OR depositor_tesim:\"#{@user.id}\")"
+      solr_parameters[:fq] ||= []
+      solr_parameters[:fq] << query
+    end
+
+    def check_for_logged_in_user
+      if current_user
+      else
+        render_404("User not signed in") and return
+      end
+    end
 
 end
