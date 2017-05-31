456631e92be3f6e64ad4e4c9b370d193b54f22ba
Eli Zoller <zoller.eli@gmail.com>
2017-05-31 15:10:49 -0400

    add news_items to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526154953) do
+ActiveRecord::Schema.define(version: 20170526180105) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",       null: false
@@ -148,6 +148,16 @@ ActiveRecord::Schema.define(version: 20170526154953) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "news_items", force: true do |t|
+    t.string   "author"
+    t.string   "publish"
+    t.string   "title",      null: false
+    t.string   "slug",       null: false
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pages", force: true do |t|
     t.string   "title",      null: false
