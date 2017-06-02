15f8e5c1e9618df61f3595c882d9ed7f3dc09325
Eli Zoller <zoller.eli@gmail.com>
2017-06-02 09:00:54 -0400

    creating menu_links activerecord objects

---
+++ db/migrate/20170601175928_create_menu_links.rb
@@ -0,0 +1,13 @@
+class CreateMenuLinks < ActiveRecord::Migration
+  def change
+    create_table :menu_links do |t|
+      t.string :link_text, :null=>false
+      t.string :link_href, :null=>false
+      t.string :classes
+      t.integer :link_order
+      t.integer :parent_link_id
+      t.string :menu_name
+      t.timestamps
+    end
+  end
+end
