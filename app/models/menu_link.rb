7f7a6fa12081f94bfc85acba83cfcaf958c65b2a
Eli Zoller <zoller.eli@gmail.com>
2017-06-05 09:50:31 -0400

    renaming menu_item to menu_link to match migration

--- app/models/menu_link.rb
+++ app/models/menu_link.rb
@@ -1,4 +1,4 @@
-class MenuItem < ActiveRecord::Base
+class MenuLink < ActiveRecord::Base
   attr_accessible :link_text, :link_href, :classes, :link_order, :parent_link_id, :menu_name if Rails::VERSION::MAJOR < 4
   validates_presence_of :link_text, :link_href, :link_order, :menu_name
 
