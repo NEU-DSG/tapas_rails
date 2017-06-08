c5804a14810cc6e8d7058195b47a9eeb46ef05ea
Eli Zoller <zoller.eli@gmail.com>
2017-06-08 11:52:24 -0400

    fix for forem layout

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -7,7 +7,7 @@ Forem.per_page = 20
 
 Rails.application.config.to_prepare do
 #   If you want to change the layout that Forem uses, uncomment and customize the next line:
-  Forem::ApplicationController.layout "application" #figure out how to change to blacklight? TODO
+  Forem::ApplicationController.layout "forem"
 #
 #   If you want to add your own cancan Abilities to Forem, uncomment and customize the next line:
 #   Forem::Ability.register_ability(Ability)
