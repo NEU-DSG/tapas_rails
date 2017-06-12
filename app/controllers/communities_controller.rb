ba699c8c02e7897edb5e161f60bc958b76b6a294
Eli Zoller <zoller.eli@gmail.com>
2017-06-12 08:11:01 -0400

    printing out env and blacklight_config in the partial since it isn't showing up in the staging log

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -45,9 +45,6 @@ class CommunitiesController < CatalogController
     authorize! :show, params[:id]
     @community = Community.find(params[:id])
     @page_title = @community.title || ""
-    logger.error "We are in the catalog index method"
-    logger.error ENV.to_yaml
-    logger.error blacklight_config
   end
 
   #This method is used to create a new community/project
