5e03af4f7459c65b9059c6ae2eb79ecdc30ee0dc
Eli Zoller <zoller.eli@gmail.com>
2017-08-20 18:47:55 +0000

    adding start of institution objects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170719221855) do
+ActiveRecord::Schema.define(version: 20170719223854) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id",       null: false
@@ -150,8 +150,8 @@ ActiveRecord::Schema.define(version: 20170719221855) do
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
 
   create_table "institutions", force: true do |t|
-    t.string   "name",        null: false
-    t.string   "description"
+    t.string   "name",                    null: false
+    t.text     "description", limit: 255
     t.string   "image"
     t.string   "address"
     t.string   "latitude"
