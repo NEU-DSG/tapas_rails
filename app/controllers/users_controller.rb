6c6de0a101366e275b14499b526b91db06d2911e
Eli Zoller <zoller.eli@gmail.com>
2017-10-04 09:55:31 -0700

    adding my tapas views and mail all users functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ class UsersController < CatalogController
   before_filter :verify_admin, :only => [:index, :show, :edit, :create]
 
   def my_tapas
+    logger.info "my tapas"
     @page_title = "My TAPAS"
     @user = current_user
     self.search_params_logic += [:my_communities_filter]
+    logger.error search_params_logic
     (@projects, @document_list) = search_results(params, search_params_logic)
     render 'my_tapas'
   end
@@ -18,6 +20,22 @@ class UsersController < CatalogController
     self.search_params_logic += [:my_communities_filter]
     (@projects, @document_list) = search_results(params, search_params_logic)
     render 'my_projects'
+  end
+
+  def my_collections
+    @page_title = "My Collections"
+    @user = current_user
+    self.search_params_logic += [:my_collections_filter]
+    (@collections, @document_list) = search_results(params, search_params_logic)
+    render 'my_collections'
+  end
+
+  def my_records
+    @page_title = "My Records"
+    @user = current_user
+    self.search_params_logic += [:my_records_filter]
+    (@records, @document_list) = search_results(params, search_params_logic)
+    render 'my_records'
   end
 
   def index
@@ -48,12 +66,57 @@ class UsersController < CatalogController
     catalog_index_path(options.except(:controller, :action))
   end
 
+  def mail_all_users
+    if params[:subject] && params[:content]
+      subj = params[:subject]
+      content = params[:content]
+      User.all.each do |u|
+        if u.email != "tapas@neu.edu"
+          Resque.enqueue(SendMailJob, u, subj, content)
+        end
+      end
+      flash[:notice] = "Mail was sent to all users"
+      redirect_to "/admin"
+    else
+      render "mail_all_users"
+    end
+  end
+
   private
     def my_communities_filter(solr_parameters, user_parameters)
       model_type = RSolr.solr_escape "info:fedora/afmodel:Community"
-      query = "has_model_ssim:\"#{model_type}\" && (project_members_ssim:\"#{@user.id}\" OR depositor_tesim:\"#{@user.id}\")"
+      query = "has_model_ssim:\"#{model_type}\" && (project_members_ssim:\"#{@user.id.to_s}\" OR depositor_tesim:\"#{@user.id.to_s}\" OR project_admins_ssim:\"#{@user.id.to_s}\" OR project_editors_ssim:\"#{@user.id.to_s}\")"
+      logger.error query
       solr_parameters[:fq] ||= []
       solr_parameters[:fq] << query
+    end
+
+    def my_collections_filter(solr_parameters, user_parameters)
+      model_type = RSolr.solr_escape "info:fedora/afmodel:Collection"
+      projects = ActiveFedora::SolrService.query("has_model_ssim:\"#{RSolr.solr_escape "info:fedora/afmodel:Community"}\" && (project_members_ssim:\"#{@user.id.to_s}\" OR depositor_tesim:\"#{@user.id.to_s}\" OR project_admins_ssim:\"#{@user.id.to_s}\" OR project_editors_ssim:\"#{@user.id.to_s}\")")
+      col_query = projects.map do |p|
+        "project_pid_ssi: #{RSolr.solr_escape(p['id'])}"
+      end
+      solr_parameters[:fq] ||= []
+      solr_parameters[:fq] << col_query.join(" OR ")
+      solr_parameters[:fq] << "has_model_ssim: \"#{model_type}\""
+    end
+
+    def my_records_filter(solr_parameters, user_parameters)
+      model_type = RSolr.solr_escape "info:fedora/afmodel:CoreFile"
+      projects = ActiveFedora::SolrService.query("has_model_ssim:\"#{RSolr.solr_escape "info:fedora/afmodel:Community"}\" && (project_members_ssim:\"#{@user.id.to_s}\" OR depositor_tesim:\"#{@user.id.to_s}\" OR project_admins_ssim:\"#{@user.id.to_s}\" OR project_editors_ssim:\"#{@user.id.to_s}\")")
+      col_query = projects.map do |p|
+        "project_pid_ssi: #{RSolr.solr_escape(p['id'])}"
+      end
+      collections = ActiveFedora.SolrService.query("has_model_ssim:\"#{RSolr.solr_escape "info:fedora/afmodel:Collection"}\" && (#{col_query.join(" OR ")})")
+      rec_query = collections.map do |y|
+        "collection_pid_ssi: #{RSolr.solr_escape(y['id'])}"
+      end
+      solr_parameters[:fq] ||= []
+      solr_parameters[:fq] << rec_query.join(" OR ")
+      solr_parameters[:fq] << "has_model_ssim: \"#{model_type}\""
+      # get projects that the user is depositor, member, admin, or editor for then find collections where user is depositor or the collection belongs to one of the projects above, then get records where depositor or the record belongs to one of the collections above
+
     end
 
     def check_for_logged_in_user
