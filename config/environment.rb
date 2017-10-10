15a50f9eae4d8287d22249b1ba7c6b439d787fed
Eli Zoller <zoller.eli@gmail.com>
2017-10-10 17:22:23 +0000

    fix typo

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Enconding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
 
 # Initialize the Rails application.
 TapasRails::Application.initialize!
