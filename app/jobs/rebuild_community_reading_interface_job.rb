54f597f3267ca9ddbed327e43a942b2013134e4e
Eli Zoller <zoller.eli@gmail.com>
2017-11-14 17:04:55 +0000

    comment out logs

--- app/jobs/rebuild_community_reading_interface_job.rb
+++ app/jobs/rebuild_community_reading_interface_job.rb
@@ -13,7 +13,7 @@ class RebuildCommunityReadingInterfaceJob
     if Community.exists?(self.pid)
       project = Community.find(self.pid)
       project.descendent_collections.each do |col|
-        logger.info "col is #{col.pid}"
+        # logger.info "col is #{col.pid}"
         # rerun the ographies first
         all_verbs = ["is_personography_for_ssim:\"info:fedora/#{col.pid}\"",
           "is_orgography_for_ssim:\"info:fedora/#{col.pid}\"",
@@ -23,19 +23,19 @@ class RebuildCommunityReadingInterfaceJob
           "is_placeography_for_ssim:\"info:fedora/#{col.pid}\"",]
 
         all_verbs = all_verbs.join(" OR ")
-        logger.info all_verbs
+        # logger.info all_verbs
         descendent_ographies = ActiveFedora::SolrService.query(all_verbs)
-        logger.info descendent_ographies
+        # logger.info descendent_ographies
         descendent_ographies.each do |file|
           if CoreFile.exists?(file['id'])
-            logger.info "ography file #{file['id']}"
+            # logger.info "ography file #{file['id']}"
             RebuildReadingInterfaceJob.perform(file['id'])
           end
         end
         # then run the rest of the files
         col.descendent_records.each do |file|
           if !file.is_ography?
-            logger.info "non ography #{file.pid}"
+            # logger.info "non ography #{file.pid}"
             RebuildReadingInterfaceJob.perform(file.pid)
           end
         end
