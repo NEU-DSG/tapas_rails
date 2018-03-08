c422b296541f38e1a4a4fe3bcbcd6513a271f1da
Eli Zoller <zoller.eli@gmail.com>
2018-03-08 12:42:51 -0700

    still trying to get travis to pick up the host

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,7 +1,7 @@
 TapasRails::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => "localhost"}
+  config.action_mailer.default_url_options = { :host => "travis.test" }
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
