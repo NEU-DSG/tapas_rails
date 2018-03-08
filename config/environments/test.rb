212945db7e5a4934c18b7e88195877005be0b402
Eli Zoller <zoller.eli@gmail.com>
2018-03-08 13:05:16 -0700

    more attempts to fix travis

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,7 +1,6 @@
 TapasRails::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => "travis.test" }
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
@@ -33,6 +32,8 @@ TapasRails::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { :host => "tapasdev.neu.edu" }
+  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
