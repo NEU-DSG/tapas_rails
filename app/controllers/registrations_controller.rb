fb29587bc01cfea13d652df9bfa4fb1e92da7412
Eli Zoller <zoller.eli@gmail.com>
2018-03-23 11:45:09 -0700

    show a flash message if the user says they are a TEI member but we can't find them in the wild apricot list

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,25 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :configure_permitted_parameters, :only => [:create, :update]
+  after_filter :check_membership, :only => [:create, :update]
 
   protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :name, :institution_id, :avatar, :bio, :account_type) }
-      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :name, :institution_id, :avatar, :remove_avatar, :bio) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :name, :institution_id, :avatar, :remove_avatar, :bio, :account_type) }
+    end
+
+    def check_membership
+      if resource.check_paid_status
+      # user has paid account
+      else
+        # user does not have paid acount
+        if resource.account_type == 'teic'
+          # but they think they do
+          flash[:error] = "The email you have provided does not match an existing TEI-C Membership. Please check your email and try again."
+        else
+          # but they already know that
+        end
+      end
     end
 end
