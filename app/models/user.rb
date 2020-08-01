f9c61110a57459bd635019a430f435c1a17b0846
Charles Pletcher <pletcher@deleuze.local>
2020-08-01 11:47:39 -0400

    Allow admin overrides on users
    Admins can set paid and admin status
    Users can update everything else on their own (which admins can also update)

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,8 +49,24 @@ class User < ActiveRecord::Base
     self.id.to_s
   end
 
+  def admin=(n)
+    if n.to_i == 0
+      update(admin_at: nil)
+    else
+      update(admin_at: Time.zone.now)
+    end
+  end
+
   def admin?
     !admin_at.nil?
+  end
+
+  def paid=(n)
+    if n.to_i == 0
+      update(paid_at: nil)
+    else
+      update(paid_at: Time.zone.now)
+    end
   end
 
   def paid_user?
