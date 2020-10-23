2baa222ac9e001408c2b3a5689467caff9268e06
Charles Pletcher <pletcher@deleuze.local>
2020-10-23 12:11:21 -0400

    Catch some bugs in core_files.js, get rendering of TEI closer to Drupal version
    We seem to be working with older/different CSS than what Drupal has --
    parts of it are being applied correctly, but some styles and
    transformations are missing

--- app/assets/javascripts/core_files.js
+++ app/assets/javascripts/core_files.js
@@ -12,64 +12,105 @@ function showLoadingSpinner() {
   LOADING_SPINNER.classList.remove('hidden')
 }
 
-function updateReadingInterface(contents) {
-  const readerNode = document.getElementById('reader-node')
+class ReadingEnvironment {
+  rawXml;
+  teiBp;
+  tapasGeneric;
 
-  if (readerNode.firstChild) {
-    readerNode.replaceChild(contents, readerNode.firstChild)
-  } else {
-    readerNode.appendChild(contents)
+  constructor(url, selectionEl, readerNode) {
+    this.url = url
+    this.selectionEl = selectionEl
+    this.readerNode = readerNode
+
+    this.selectionEl.addEventListener('change', this.updateXml.bind(this))
+
+    this.updateXml()
   }
 
-  hideLoadingSpinner()
-}
-
-function readingEnvAfterLoad() {
-  let rawXml = ''
-  let styledTei = ''
-
-  const canonicalObjectUrl = document.getElementById('core-file-url').dataset.url
-  const selectionEl = document.getElementById('reading_selector') || {}
-
-  async function updateXml() {
-    showLoadingSpinner()
-
-    const selectedOption = selectionEl.selectedOptions && selectionEl.selectedOptions[0] || {}
-    const selectedValue = selectedOption.value || 'tei2html'
-
-    if (selectedValue === 'teibp') {
-      if (rawXml === '') {
-        const resp = await fetch(canonicalObjectUrl)
-        const xml = await resp.text()
-
-        rawXml = xml
-      }
-
+  async handleRawXml() {
+    if (!this.rawXml) {
+      const resp = await fetch(this.url)
+      const xml = await resp.text()
       const pre = document.createElement('pre')
-      pre.innerText = rawXml
-      return updateReadingInterface(pre)
+      pre.innerText = xml;
+      this.rawXml = pre
     }
 
-    if (styledTei === '') {
+    return this.rawXml
+  }
+
+  // [["TAPAS", 'tei2html'], ["Raw XML", 'raw'], ['TEI', 'teibp']]
+  async handleSelection(selection = 'tei2html') {
+    if (selection === 'raw') {
+      return this.handleRawXml()
+    } else if (selection === 'tei2html') {
+      return this.handleTapas()
+    } else if (selection === 'teibp') {
+      return this.handleTeiBoilerplate()
+    }
+  }
+
+  async handleTapas() {
+    if (!this.tapasGeneric) {
       const doc = await SaxonJS.transform({
-        sourceLocation: canonicalObjectUrl,
-        stylesheetLocation: `/assets/${selectedValue}.sef.json`
+        sourceLocation: this.url,
+        // TODO: (charles) Maybe don't hardcode as many of these locations?
+        stylesheetLocation: '/view_packages/tapas-generic/tei2html.sef.json',
+        stylesheetParams: {
+          'Q{}assets-base': '/view_packages/tapas-generic/',
+          tapasHome: window.location,
+        }
       }, 'async')
 
-      styledTei = doc.principalResult
+      this.tapasGeneric = doc.principalResult
     }
 
-    updateReadingInterface(styledTei)
+    return this.tapasGeneric
   }
 
-  selectionEl.addEventListener('change', updateXml);
-  updateXml();
+  async handleTeiBoilerplate() {
+    if (!this.teiBp) {
+      const doc = await SaxonJS.transform({
+        sourceLocation: this.url,
+        stylesheetLocation: '/view_packages/teibp/teibp.sef.json',
+      }, 'async')
+
+      this.teiBp = doc.principalResult
+    }
+
+    return this.teiBp
+  }
+
+  async updateXml() {
+    showLoadingSpinner()
+
+    const selectedValue = (this.selectionEl.selectedOptions && this.selectionEl.selectedOptions[0] || {}).value
+    const contents = await this.handleSelection(selectedValue)
+
+    this.updateReadingInterface(contents)
+
+    hideLoadingSpinner()
+  }
+
+  updateReadingInterface(contents) {
+    if (this.readerNode.firstChild) {
+      this.readerNode.replaceChild(contents, this.readerNode.firstChild)
+    } else {
+      this.readerNode.appendChild(contents)
+    }
+  }
+}
+
+function initializeReadingEnv() {
+  const url = document.getElementById('core-file-url').dataset.url
+  const selectionEl = document.getElementById('reading_selector')
+  const readerNode = document.getElementById('reader-node')
+
+  window.TAPAS_READING_ENVIRONMENT = new ReadingEnvironment(url, selectionEl, readerNode)
 }
 
 (function($) {
-  $(document).ready(function() {
-    readingEnvAfterLoad()
-  });
+  $(document).ready(initializeReadingEnv);
 })(jQuery);
 
 jQuery.browser = {};
