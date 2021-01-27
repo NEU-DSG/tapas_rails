191f325dac588aee348fc1d2f84ba158869bcabd
Charles Pletcher <pletcher@deleuze.local>
2021-01-27 13:12:03 -0500

    Fix up Community spec, testing all custom methods and read permissions
    Remove kwarg-overloaded file attachment test, substituting an alias for
    ActiveStorage's own methods

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,17 +3,20 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :read, :all
+    can :read, :all, is_public: true
 
     if user.admin?
       can :manage, :all
     else
       can :manage, Community, depositor_id: user.id
       can :manage, Community, community_members: { member_type: ["editor", "admin"], user_id: user.id }
+      can :read, Community, community_members: { user_id: user.id }
       can :manage, Collection, depositor_id: user.id
       can :manage, Collection, community: { community_members: { member_type: ["editor", "admin"], user_id: user.id } }
+      can :read, Collection, community: { community_members: { user_id: user.id } }
       can :manage, CoreFile, depositor_id: user.id
       can :manage, CoreFile, collections: { community: { community_members: { member_type: ["editor", "admin"], user_id: user.id } } }
+      can :read, CoreFile, collections: { community: { community_members: { user_id: user.id } } }
     end
   end
 end
