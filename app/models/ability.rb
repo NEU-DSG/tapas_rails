76d2768405807b2df3a07aa449b89dc5d7582e38
Charles Pletcher <pletcher@deleuze.local>
2021-01-29 17:51:13 -0500

    Add tests for CollectionsController

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,18 +5,25 @@ class Ability
     user ||= User.new # guest user (not logged in)
     can :read, :all, is_public: true
 
-    if user.admin?
-      can :manage, :all
-    else
-      can :manage, Community, depositor_id: user.id
-      can :manage, Community, community_members: { member_type: ["editor", "admin"], user_id: user.id }
-      can :read, Community, community_members: { user_id: user.id }
-      can :manage, Collection, depositor_id: user.id
-      can :manage, Collection, community: { community_members: { member_type: ["editor", "admin"], user_id: user.id } }
-      can :read, Collection, community: { community_members: { user_id: user.id } }
-      can :manage, CoreFile, depositor_id: user.id
-      can :manage, CoreFile, collections: { community: { community_members: { member_type: ["editor", "admin"], user_id: user.id } } }
-      can :read, CoreFile, collections: { community: { community_members: { user_id: user.id } } }
-    end
+    return if user.id.nil?
+
+    can :read, Community, community_members: { user_id: user.id }
+    can :read, Collection, community: { community_members: { user_id: user.id } }
+    can :read, CoreFile, collections: { community: { community_members: { user_id: user.id } } }
+
+    can :create, Community
+    can :create, Collection
+    can :create, CoreFile
+
+    can :manage, Community, depositor_id: user.id
+    can :manage, Community, community_members: { member_type: ["editor", "admin"], user_id: user.id }
+    can :manage, Collection, depositor_id: user.id
+    can :manage, Collection, community: { community_members: { member_type: ["editor", "admin"], user_id: user.id } }
+    can :manage, CoreFile, depositor_id: user.id
+    can :manage, CoreFile, collections: { community: { community_members: { member_type: ["editor", "admin"], user_id: user.id } } }
+
+    return unless user.admin?
+
+    can :manage, :all
   end
 end
