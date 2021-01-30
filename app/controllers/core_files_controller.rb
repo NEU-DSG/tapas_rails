079890bd41a514cf6b9b33fa1b8dec2d3ec62879
Charles Pletcher <pletcher@deleuze.local>
2021-01-29 20:44:02 -0500

    Add CoreFilesController specs

--- app/controllers/core_files_controller.rb
+++ app/controllers/core_files_controller.rb
@@ -7,43 +7,47 @@ class CoreFilesController < ApplicationController
     identifier { ignore! }
   end
 
-  before_action :can_edit?, only: [:edit, :update, :destroy]
-  before_action :can_read?, :only => :show
+  before_action :authorize_edit!, only: %i[edit update destroy]
+  before_action :authorize_read!, only: :show
 
   def index
-    @page_title = "All CoreFiles"
+    @page_title = 'All CoreFiles'
 
     @search = CoreFileSearch.new(params)
     @results = @search.result
 
     respond_to do |format|
-      format.html { render :template => 'shared/index' }
-      format.js { render :template => 'shared/index', :layout => false }
+      format.html { render template: 'shared/index' }
+      format.js { render template: 'shared/index', layout: false }
     end
   end
 
   def new
-    @page_title = "Create New Record"
-    @collections = Collection.accessible_by(current_ability)
     @core_file = CoreFile.new(is_public: true)
+
+    authorize! :new, @core_file
+
+    @page_title = 'Create New Record'
+    @collections = Collection.accessible_by(current_ability)
     @users = User.order(:name)
   end
 
   def create
+    authorize! :manage, CoreFile
+
     # a user can't be both a contributor and an author; just assume that they're an author
     params[:core_file][:contributor_ids] = params[:core_file][:contributor_ids] - params[:core_file][:author_ids]
 
-    file = CoreFile.create!(core_file_params.merge({ depositor_id: current_user.id }))
-    file.set_authors(params[:core_file][:author_ids])
+    @file = CoreFile.create!(core_file_params.merge(depositor: current_user))
+    @file.set_authors(params[:core_file][:author_ids])
 
-    redirect_to file
+    redirect_to @file
   end
 
   def destroy
     file = CoreFile.find(params[:id])
     # FIXME: (charles) Should go to the collection where the user is, but the routes aren't set up RESTfully
     collection = file.collections.kept.first
-    puts collection.id
     file.destroy!
 
     redirect_to collection
@@ -66,35 +70,35 @@ class CoreFilesController < ApplicationController
   def view_package_html
     @core_file = CoreFile.find_by_did(params[:did])
     if @core_file.blank?
-      render :text => "Resource not found", :status => 404
+      render text: 'Resource not found', status: 404
     else
       @core_file.create_view_package_methods
-      view_package = ViewPackage.where(:machine_name => "#{params[:view_package]}").to_a.first
+      view_package = ViewPackage.where(machine_name: "#{params[:view_package]}").to_a.first
       if !view_package.blank?
         e = "Could not find a #{view_package.human_name} representation of this object."\
-          "Please retry in a few minutes."
+          'Please retry in a few minutes.'
         html = @core_file.send("#{view_package.machine_name}".to_sym)
         render_content_asset html, e
       else
-        render :text => "The view package #{params[:view_package]} could not be found", :status => 422
+        render text: "The view package #{params[:view_package]} could not be found", status: 422
       end
     end
   end
 
   def mods
     @html = render_mods_display(@core_file).to_html
-    render :text => @html
+    render text: @html
   end
 
   def tei
-    e = "Could not find TEI associated with this file.  Please retry in a "\
-      "few minutes and contact an administrator if the problem persists."
+    e = 'Could not find TEI associated with this file.  Please retry in a '\
+      'few minutes and contact an administrator if the problem persists.'
     render_content_asset @core_file.canonical_object, e
   end
 
   def rebuild_reading_interfaces
     RebuildReadingInterfaceJob.perform(params[:did])
-    @response[:message] = "Record updated successfully"
+    @response[:message] = 'Record updated successfully'
     pretty_json(200) and return
   end
 
@@ -103,12 +107,12 @@ class CoreFilesController < ApplicationController
 
     if @core_file.collections.empty?
       @core_file.discard
-      return render_404 "We could not find that file."
+      return render_404 'We could not find that file.'
     end
 
     # TODO: (charles) ['TEI', 'teibp'] currently hard codes asset locations. Add to @view_packages
     # array once those locations have been parameterized
-    @view_packages = [["TAPAS", 'tei2html'], ["TEI", 'teibp'], ["Hieractivity", 'hieractivity'], ["Raw XML", 'raw']]
+    @view_packages = [['TAPAS', 'tei2html'], ['TEI', 'teibp'], ['Hieractivity', 'hieractivity'], ['Raw XML', 'raw']]
   end
 
   def api_show
@@ -156,10 +160,10 @@ class CoreFilesController < ApplicationController
       # file upload
       if params[:tei]
         opts = {
-          :authors => params[:display_authors],
-          :contributors => params[:display_contributors],
-          :"timeline-date" => params[:display_date],
-          :title => params[:title]
+          authors: params[:display_authors],
+          contributors: params[:display_contributors],
+          "timeline-date": params[:display_date],
+          title: params[:title]
         }
 
         @mods = Exist::GetMods.execute(params[:tei], opts)
@@ -172,9 +176,9 @@ class CoreFilesController < ApplicationController
       # Step 5: Respond with MODS if it is available, otherwise send a generic
       # success message
       if @mods
-        render :xml => @mods, :status => 202
+        render xml: @mods, status: 202
       else
-        @response[:message] = "Job processing"
+        @response[:message] = 'Job processing'
         pretty_json(202) and return
       end
     rescue => e
@@ -187,6 +191,14 @@ class CoreFilesController < ApplicationController
   end
 
   protected
+
+  def authorize_edit!
+    authorize! :manage, CoreFile.find(params[:id])
+  end
+
+  def authorize_read!
+    authorize! :read, CoreFile.find(params[:id])
+  end
 
   def can_edit?
     can? :manage, CoreFile.find(params[:id])
@@ -203,10 +215,10 @@ class CoreFilesController < ApplicationController
       :description,
       :featured,
       :title,
-      :author_ids => [],
-      :collection_ids => [],
-      :contributor_ids => [],
-      :thumbnails => []
+      author_ids: [],
+      collection_ids: [],
+      contributor_ids: [],
+      thumbnails: []
     )
   end
 
@@ -214,9 +226,9 @@ class CoreFilesController < ApplicationController
 
   def render_content_asset(asset, error_msg)
     if asset && asset.content.content.present?
-      render :text => asset.content.content
+      render text: asset.content.content
     else
-      render :text => error_msg, :status => 404
+      render text: error_msg, status: 404
     end
   end
 end
--- spec/controllers/core_files_controller_spec.rb
+++ spec/controllers/core_files_controller_spec.rb
@@ -1,5 +1,165 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe CoreFilesController do
-  skip
+  include ControllerHelpers
+
+  let(:institution) { FactoryBot.create(:institution) }
+  let(:user) { FactoryBot.create(:user, institution: institution) }
+  let(:contributor) { FactoryBot.create(:user, institution: institution) }
+
+  let(:community) { FactoryBot.create(:community, depositor: user) }
+  let(:collection) { FactoryBot.create(:collection, community: community, depositor: user) }
+
+  def params
+    {
+      core_file: {
+        description: "it's a core file",
+        featured: false,
+        title: 'a title',
+        author_ids: [user.id],
+        collection_ids: [collection.id],
+        contributor_ids: [contributor.id]
+      }
+    }
+  end
+
+  describe '#index' do
+    it 'renders the CoreFiles' do
+      core_files = FactoryBot.create_list(:core_file, 3, collections: [collection])
+
+      get :index
+
+      expect(assigns(:results)).to eq(core_files)
+      expect(response).to render_template('shared/index')
+    end
+  end
+
+  describe '#new' do
+    it 'does not allow non-logged-in users' do
+      expect { get :new }.to raise_error(CanCan::AccessDenied)
+    end
+
+    it 'allows logged-in users' do
+      login_user
+
+      get :new
+
+      expect(response).to render_template('new')
+    end
+  end
+
+  describe '#create' do
+    it 'does not allow non-logged-in users' do
+      expect { post :create, params: params }.to raise_error(CanCan::AccessDenied)
+    end
+
+    it 'allows logged-in users' do
+      login_user
+
+      post :create, params: params
+
+      expect(response).to redirect_to("/core_files/#{assigns(:file).id}")
+    end
+  end
+
+  describe '#show' do
+    let(:file) { FactoryBot.create(:core_file, collections: [collection], depositor: user) }
+
+    it 'renders the requested file' do
+      get :show, params: { id: file.id }
+
+      expect(assigns(:core_file)).to eq(file)
+      expect(response).to render_template('show')
+    end
+  end
+
+  describe '#destroy' do
+    let(:file) { FactoryBot.create(:core_file, collections: [collection], depositor: user) }
+
+    it 'does not allow non-logged-in users' do
+      expect { delete :destroy, params: { id: file.id } }.to raise_error(CanCan::AccessDenied)
+    end
+
+    it 'does not allow unauthorized users' do
+      login_user
+
+      expect { delete :destroy, params: { id: file.id } }.to raise_error(CanCan::AccessDenied)
+    end
+
+    it 'allows authorized users' do
+      login_user(user)
+
+      delete :destroy, params: { id: file.id }
+
+      expect(response).to redirect_to("/collections/#{file.collections.first.id}")
+    end
+
+    it 'allows admins' do
+      login_admin
+
+      delete :destroy, params: { id: file.id }
+
+      expect(response).to redirect_to("/collections/#{file.collections.first.id}")
+    end
+  end
+
+  describe '#edit' do
+    let(:file) { FactoryBot.create(:core_file, collections: [collection], depositor: user) }
+
+    it 'does not allow un-logged-in users' do
+      expect { get :edit, params: { id: file.id } }.to raise_error(CanCan::AccessDenied)
+    end
+
+    it 'does not allow unauthorized users' do
+      login_user
+
+      expect { get :edit, params: { id: file.id } }.to raise_error(CanCan::AccessDenied)
+    end
+
+    it 'allows authorized users' do
+      login_user(user)
+
+      get :edit, params: { id: file.id }
+
+      expect(response).to render_template('edit')
+    end
+
+    it 'allows admins' do
+      login_admin
+
+      get :edit, params: { id: file.id }
+
+      expect(response).to render_template('edit')
+    end
+  end
+
+  describe '#update' do
+    let(:file) { FactoryBot.create(:core_file, collections: [collection], depositor: user) }
+
+    it 'does not allow non-logged-in users' do
+      expect { put :update, params: params.merge(id: file.id) }.to raise_error(CanCan::AccessDenied)
+    end
+
+    it 'does not allow unauthorized users' do
+      login_user
+
+      expect { put :update, params: params.merge(id: file.id) }.to raise_error(CanCan::AccessDenied)
+    end
+
+    it 'allows authorized users' do
+      login_user(user)
+
+      put :update, params: params.merge(id: file.id)
+
+      expect(response).to redirect_to("/core_files/#{file.id}")
+    end
+
+    it 'allows admins' do
+      login_admin
+
+      put :update, params: params.merge(id: file.id)
+
+      expect(response).to redirect_to("/core_files/#{file.id}")
+    end
+  end
 end
