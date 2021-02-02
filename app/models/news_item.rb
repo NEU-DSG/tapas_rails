4a94b2c3b190ad95ea0cc90072c8cf7f5fe59898
Luke Hollis <lukehollis@gmail.com>
2021-02-01 21:17:09 -0800

    remove indexing news items in solr on save

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -6,23 +6,4 @@ class NewsItem < ActiveRecord::Base
   validates :slug, uniqueness: { case_sensitive: false }
   friendly_id :slug, use: :slugged
 
-  after_save :index_record
-  before_destroy :remove_from_index
-
-  def to_solr
-    obj =
-    {'id' => self.id,
-     'title_info_title_ssi' => self.title,
-     'all_text_timv' => self.content,
-     'type_sim' => 'News Item',
-     'active_fedora_model_ssi' => 'NewsItem',
-     'creator_tesim' => self.author,
-     'edit_access_person_ssim' => self.author
-   }
-   if self.publish == "true"
-     obj['read_access_group_ssim'] = ['public']
-   end
-   return obj
-  end
-
 end
