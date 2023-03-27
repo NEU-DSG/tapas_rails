502ed1d048c1fbe61628f10ddb11fee67b0340db
candyhazlett <candaces@bu.edu>
2023-03-27 11:12:15 -0400

    modifies config file using the rails update task

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,5 @@
-TapasRails::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
@@ -12,11 +11,12 @@ TapasRails::Application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
-  config.log_level = :debug
 
-  # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_assets  = true
-  config.static_cache_control = "public, max-age=3600"
+  # Configure public file server for tests with Cache-Control for performance.
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
+  }
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
@@ -28,22 +28,19 @@ TapasRails::Application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  # Store uploaded files on the local file system in a temporary directory
+  config.active_storage.service = :test
+
+  config.action_mailer.perform_caching = false
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: "tapasdev.neu.edu" }
-  config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
-  config.active_storage.service = :test
-
-  config.middleware.use ExceptionNotification::Rack,
-    :email => {
-      :email_prefix => "[Tapas Rails Notifier TEST]",
-      :sender_address => %{"notifier" <notifier@tapasrailsdev.neu.edu>},
-      :exception_recipients => %w{nobody@nowhere.com}
-    }
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 end
