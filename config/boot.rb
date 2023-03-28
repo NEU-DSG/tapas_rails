ff336170535c435dd39b3a541b889f3f1f8ae8b8
candyhazlett <candaces@bu.edu>
2023-03-28 12:28:02 -0400

    updates gitignore file and boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,12 +1,12 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
-
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-
-
 # pre-upgrade - 03272023 by CCH
+
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 #
-# # Set up gems listed in the Gemfile.
-# ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-#
-# require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+# require 'bundler/setup' # Set up gems listed in the Gemfile.
+# require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
