b8b1fbfcee0944a5ef6971538de74ab78d775f8a
candyhazlett <candaces@bu.edu>
2023-03-31 11:48:46 -0400

    removes forum/forem migrations, html template elements, references in classes, styling, controllers, config files, and js files related to bootsy

--- app/decorators/controllers/forem/application_controller_decorator.rb
+++
@@ -1,6 +0,0 @@
-Forem::ApplicationController.class_eval do
-  def search_action_url(options = {})
-    # Rails 4.2 deprecated url helpers accepting string keys for 'controller' or 'action'
-    "/catalog"
-  end
-end
--- db/migrate/20170425151606_create_bootsy_img_galleries.bootsy.rb
+++
@@ -1,9 +0,0 @@
-# This migration comes from bootsy (originally 20120628124845)
-class CreateBootsyImgGalleries < ActiveRecord::Migration[5.2]
-  def change
-    create_table :bootsy_img_galleries do |t|
-      t.references :bootsy_resource, polymorphic: true
-      t.timestamps
-    end
-  end
-end
