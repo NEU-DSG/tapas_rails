172253e5eedf2fd8d4cc19592f9818afc5b86109
Ash Clark <amclark42@gmail.com>
2024-08-19 17:01:44 -0400

    Adjust scroll button intersection observer: test the reader header.
    Instead of testing the *page* header, we test the reader header for
    an intersection threshold of 1.0. Thanks to a new `top: -1px` rule,
    we now know that when the reader header is sticky, a part of it is
    just out of the viewport.
    
    I also set up an `.is-sticking` class on the reader header,
    preparing to maybe reduce the amount of space the sticky header
    takes up.

--- assets/reader.js
+++ assets/reader.js
@@ -17,18 +17,22 @@ tapas.reader = {};
    **/
   
   /*
-    Conditionally set the visibility of the "scroll to top" button, depending on where on the page the user is. This is a callback function for an IntersectionObserver.
+    Conditionally set the visibility of the "scroll to top" button, depending on where on the page the 
+    user is. This is a callback function for an IntersectionObserver.
    */
   let setScrollButtonVisibility = function (entries, observer) {
     entries.forEach( entry => {
       let scrollDiv = document.getElementById('jump-to-top').closest('div');
-      console.log(scrollDiv);
-      /* If the target entry (the header) is inside the viewport, make sure the button only appears in document order. */
-      if ( entry.isIntersecting ) {
-        scrollDiv.classList.remove('jump-sticky');
-      /* If the target entry is NOT in view, use "fixed" position to show the button at the bottom of the screen. */
-      } else {
+      /* If the target entry (the header) is inside the viewport, make sure the button only appears in 
+        document order. Also, add the `.is-sticking` class to the entry element. */
+      if ( entry.intersectionRatio < 1 ) {
+        entry.target.classList.add('is-sticking');
         scrollDiv.classList.add('jump-sticky');
+      /* Otherwise, use "sticky" position to show the button at the bottom of the screen, and remove the 
+        `.is-sticking` class from the entry element. */
+      } else {
+        entry.target.classList.remove('is-sticking');
+        scrollDiv.classList.remove('jump-sticky');
       }
     });
   };
@@ -38,10 +42,21 @@ tapas.reader = {};
       PUBLIC FUNCTIONS
    **/
   
+  /*
+    Set up an Intersection Observer which will add the `.jump-sticky` class to the "Return to top" 
+    container when `.header` scrolls out of view. This solution owes a great deal to "How to Make an 
+    Unobtrusive Scroll-to-Top Button" by Marcel Rojas 
+    (https://css-tricks.com/how-to-make-an-unobtrusive-scroll-to-top-button/).
+   */
   this.setUpScrollButton = function () {
-    let observer = new IntersectionObserver(setScrollButtonVisibility),
-        pgHeader = document.getElementById('header');
-    observer.observe(pgHeader);
+    /* We're only interested in tracking when the `.reader-header` intersection ratio changes to or from 
+      1.0 (fully visible). When it's below 1, the header's "sticky" position is kicking in. (We know 
+      this because `.reader-header` is set to `top: -1px;`, meaning that when the header is behaving 
+      "stickily", a single pixel is out of the viewport — and the ratio is less than 1.0.) */
+    let ratioThresholds = [ 1 ],
+        observer = new IntersectionObserver(setScrollButtonVisibility, { threshold: ratioThresholds }),
+        readerHeader = document.querySelector('.reader-header');
+    observer.observe(readerHeader);
   }
   
 }).apply(tapas.reader); // Apply the namespace to the anonymous function.
