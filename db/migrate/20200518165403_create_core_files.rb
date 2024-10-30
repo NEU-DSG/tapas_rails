4d978479f23dd6405c0176c4f59a6852e864d7bf
candyhazlett <candaces@bu.edu>
2024-10-30 11:35:43 -0400

    removes collections_core_files join table in separate migration

--- db/migrate/20200518165403_create_core_files.rb
+++ db/migrate/20200518165403_create_core_files.rb
@@ -5,12 +5,5 @@ class CreateCoreFiles < ActiveRecord::Migration[5.2]
       t.text :description
       t.timestamps
     end
-
-    create_table :collections_core_files do |t|
-      t.belongs_to :core_file
-      t.belongs_to :collection
-    end
-
-    add_index :collections_core_files, [:collection_id, :core_file_id], unique: true
   end
 end
