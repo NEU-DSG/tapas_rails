d737a841427ea5dbe9d25c60740fe683b3ebcd24
candyhazlett <candaces@bu.edu>
2024-11-07 11:32:21 -0500

    adds migrations for project_core_file, collection_core_file migrations to create new tables, and a migration for adding an image_url attribute/column to image_file model/table, adds helper method to process_image_data and uncomments file_format method to display an error when user attempts to upload an unsupported file type to image_file class

--- app/models/image_file.rb
+++ app/models/image_file.rb
@@ -1,18 +1,20 @@
 class ImageFile < ActiveRecord::Base
   # associations
   belongs_to :imageable, polymorphic: true
-
-  has_one_attached :file
-
   belongs_to :depositor, class_name: "User"
+  has_one_attached :file
 
   # validations
   validates_presence_of :title, :depositor_id
-  # validate :file_format
-  #
-  # def file_format
-  #   return unless file.attached?
-  #
-  #   errors.add(:image, "must be a JPEG, JPG, GIF, or PNG file") unless image.content_type.in?(%w[image/jpeg image/jpg image/gif image/png])
-  # end
+  validate :file_format
+
+  def process_image_data
+    URI.open(image_url)
+  end
+
+  def file_format
+    return unless file.attached?
+
+    errors.add(:image, "must be a JPEG, JPG, GIF, or PNG file") unless content_type.in?(%w[image/jpeg image/jpg image/gif image/png])
+  end
 end
