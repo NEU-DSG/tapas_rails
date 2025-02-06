c3f73b1300cc75849ae2e6c6379d3773667020dc
Ash Clark <amclark42@gmail.com>
2025-02-06 15:50:46 -0500

    Delete some unused CSS and SCSS files.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,7 @@
 //= require popper
 //= require bootstrap
-//= require blacklight/blacklight
+/* TODO: ActiveStorage JS allows for uploading directly to the cloud. 
+    https://api.rubyonrails.org/classes/ActiveStorage.html#module-ActiveStorage-label-Direct+uploads
+    We may want to proxy instead */
 //= require activestorage
 //= require tapas
