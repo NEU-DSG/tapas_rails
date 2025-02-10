9fd0453a6f359160ca1796509b0c35152c462995
Ash Clark <amclark42@gmail.com>
2025-02-10 15:40:47 -0500

    Clean up routes a little.
    Removed a couple routes I know we're getting rid of. Condensed the
    routes for Projects, Collections, and Core Files. Also, added `:`
    to identifiers in the route path definitions.
    
    Eventually we'll need more complex URLs for projects, etc., but it
    looks like Rails 7 has more powerful routing capabilities. It might
    be good to wait until we've upgraded.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TapasRails::Application.routes.draw do
+  
   root :to => "catalog#browse"
 
   resources :catalog, controller: 'catalog', only: [:index, :show] do
@@ -15,13 +16,11 @@ TapasRails::Application.routes.draw do
   }
 
   get 'my_tapas' => 'users#my_tapas'
-  get 'my_projects' => 'users#my_projects'
-  get 'my_collections' => 'users#my_collections'
-  get 'my_records' => 'users#my_records'
+  # 2025-02: removed "my_projects", "my_collections", and "my_records"
   get 'admin/users/new' => 'users#admin_new', as: 'admin_new_user'
-  get 'admin/users/id' => 'users#admin_show'
+  get 'admin/users/:id' => 'users#admin_show'
   post 'admin/users' => 'users#admin_create', as: 'admin_create_user'
-  get 'users/id' => 'users#profile'
+  get 'users/:id' => 'users#profile'
   get 'mail_users' => 'users#mail_all_users', as: 'mail_users'
   post 'mail_users' => 'users#mail_all_users'
 
@@ -37,37 +36,38 @@ TapasRails::Application.routes.draw do
   get 'browse' => 'catalog#browse'
 
   # Projects, formerly 'Communities'
-  resources :projects
-  get 'projects/id' => 'projects#show'
-  post "projects/id" => "projects#upsert"
-  get 'projects/id/edit' => 'projects#edit'
-  get 'projects' => 'projects#index'
-  get '/catalog/id' => 'projects#show'
-  delete "projects/id" => "projects#destroy"
+  post "projects/:id" => "projects#upsert" # TODO: upsert -> update
+  resources :projects, except: :update
+  # TODO: add more complex routing to reflect ordered hierarchy, human-readable names
+  #get 'projects/:id' => 'projects#show'
+  #get 'projects/:id/edit' => 'projects#edit'
+  #get 'projects' => 'projects#index'
+  #get '/catalog/:id' => 'projects#show'
+  #delete "projects/:id" => "projects#destroy"
 
   # Collections
-  resources :collections
-  get 'collections/id' => 'collections#show'
-  post 'collections/id' => 'collections#upsert'
-  get 'collections/id/edit' => 'collections#edit'
-  get 'collections' => 'collections#index'
-  get '/catalog/id' => 'collections#show'
-  delete 'collections/id' => 'collections#destroy'
+  post 'collections/:id' => 'collections#upsert'
+  resources :collections, except: :update
+  #get 'collections/id' => 'collections#show'
+  #get 'collections/id/edit' => 'collections#edit'
+  #get 'collections' => 'collections#index'
+  #get '/catalog/id' => 'collections#show'
+  #delete 'collections/id' => 'collections#destroy'
 
   # CoreFiles
   resources :core_files
-  get 'core_files/id/edit' => 'core_files#edit'
-  get 'core_files' => 'core_files#index'
-  get 'core_files/new' => 'core_files#new'
+  #get 'core_files/id/edit' => 'core_files#edit'
+  #get 'core_files' => 'core_files#index'
+  #get 'core_files/new' => 'core_files#new'
 
   # get 'files/:did/mods' => 'core_files#mods'
   # get 'files/:did/tei' => 'core_files#tei'
   # get 'files/:did' => 'core_files#api_show'
-  get 'core_files/id' => 'core_files#show'
+  #get 'core_files/id' => 'core_files#show'
   # put 'core_files/:did/reading_interfaces' => 'core_files#rebuild_reading_interfaces'
-  post 'core_files/id' => 'core_files#update'
-  post 'files/id' => 'core_files#upsert'
-  delete "files/id" => "core_files#destroy"
+  #post 'core_files/id' => 'core_files#update'
+  #post 'files/id' => 'core_files#upsert'
+  #delete "files/id" => "core_files#destroy"
 
   # get 'files/:did/html/:view_package' => 'core_files#view_package_html'
 
