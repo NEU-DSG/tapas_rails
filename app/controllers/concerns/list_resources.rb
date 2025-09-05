ff72acbf80c9dfcc1933af8de1f5a7e475b0eb44
Ash Clark <amclark42@gmail.com>
2025-09-05 17:06:34 -0400

    Move sort method checking into new method `set_up_browse`.
    We'll need to check and sign off on the user's requested sort
    method for the other Browse pages too, so the new method is in the
    ListResources concern.
    
    I also added a `sort_string` method for convenience in actually
    performing a sort, and switched from camelCase to snake_case
    variable names.

--- app/controllers/concerns/list_resources.rb
+++ app/controllers/concerns/list_resources.rb
@@ -1,11 +1,31 @@
+# Methods common to the Browse pages (and possibly Search)
 module ListResources
   extend ActiveSupport::Concern
   
+  DEFAULT_SORT = 'updated_at'
+  
+  # Set up the variables we'll need for constructing a Browse page. This method has the option to
+  # include the request parameters in case we need specialized parameters for a particular kind of model.
+  def set_up_browse(params = browse_params)
+    sort_param = params[:sort]
+    # If the user requested a valid sort method, use that. Otherwise, use the default method.
+    @sort_method = is_valid_sort_method(sort_param) ? sort_param : DEFAULT_SORT
+    # Decide for the user whether to apply the sort method in ascending or descending order.
+    @sort_direction = @sort_method == 'updated_at' ? 'DESC' : 'ASC'
+  end
+  
+  def sort_string
+    @sort_method+" "+@sort_direction
+  end
+  
+  # Test a requested sort method string against the methods we are prepared to accept. Returns true ONLY 
+  # if the request parameter is provided and its lower-cased value matches one of the approved strings.
   def is_valid_sort_method(method = browse_params[:sort])
     allowed_methods = %w(title updated_at)
     !method.blank? && allowed_methods.include?(method.to_s.downcase)
   end
   
+  # For requests to a Browse page, permit only the sort parameter.
   def browse_params
     params.permit(:sort)
   end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,14 +11,10 @@ class ProjectsController < ApplicationController
   # self.search_params_logic += [:add_access_controls_to_solr_params]
   
   def browse
-    # Check for a sort request parameter.
-    sortParam = browse_params[:sort]
-    # If a sort method is requested and is an allowed value, we use that. Otherwise, fall back on the 
-    # updated timestamp.
-    @sortMethod = is_valid_sort_method(sortParam) ? sortParam : 'updated_at'
-    # Decide for the user whether to apply the sort method in ascending or descending order.
-    sortDirection = @sortMethod == 'updated_at' ? 'DESC' : 'ASC'
-    @projects = Project.publicly_visible.includes(:collections, :core_files).order(@sortMethod+" "+sortDirection)
+    # The setup method defines @sort_method and @sort_direction for ListResource's sort_string() and the
+    # Browse views.
+    set_up_browse
+    @projects = Project.publicly_visible.includes(:collections, :core_files).order(sort_string)
     render 'browse'
   end
   
